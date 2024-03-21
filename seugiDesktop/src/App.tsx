9f41e831cc6741189763fd16ad9865b4a1920de3
je355 <chaeeehui@gmail.com>
2024-03-21 23:19:56 +0900

    ⚙ ::	프로젝트 세팅
    프로젝트세팅해보앗습니다...........

---
+++ seugiDesktop/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>야호</>
+  )
+}
+
+export default App
