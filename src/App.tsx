c79d515b6571c8936a860ace05d4311de52048d8
jaeuk16 <141097687+jaeuk16@users.noreply.github.com>
2024-03-26 23:12:04 +0900

    ⚡ :: 로그인 웹 뷰 짜기

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import Router from '../src/components/router';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>야호</>
+    <Router />
   )
 }
 
-export default App
+export default App;
