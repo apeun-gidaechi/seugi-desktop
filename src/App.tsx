6dc9b2fee2afd0265788bab983b9f0ad06bcdb87
jaeuk16 <141097687+jaeuk16@users.noreply.github.com>
2024-03-26 23:43:11 +0900

    ⚡ :: 로그인 웹 뷰 짜기

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-<<<<<<< HEAD
     <Router />
-=======
-    <>메인테스트입니다.</>
->>>>>>> 0992ce8908d2f97c9d000aa3fd6617fc14331d86
   )
 }
 
--- src/components/auth/Login/login.tsx
+++ src/components/auth/Login/login.tsx
@@ -6,11 +6,11 @@ const login = () => {
   return (
     <div>
       <div>
-        <div>
-          <img src="src/assets/image/seugi.png" className="seugilogo"/> <a> 반가워요! </a>
+        <div className="header">
+          <img src="src/assets/image/Onboaring/AppIcon.svg"/> <a> 반가워요! </a>
         </div>
         <div>
-          <div>이메일 <a>*</a></div>
+          <div className="">이메일 <a>*</a></div>
           <input 
             placeholder="이메일을 입력해주세요" 
             type="email"
@@ -32,11 +32,11 @@ const login = () => {
           clientId="com.example.app" //clientId:  애플 개발자 계정에서 생성한 앱의 고유 식별자입니다. 'com.example.app'은 실제 애플 개발자 계정에서 발급받은 앱 ID로 변경해야 합니다.
           redirectURI="https://example.com/auth/apple/callback" // redirectURI: 사용자가 애플 로그인을 성공적으로 완료한 후 리디렉션될 웹 페이지의 URL입니다. 실제 서비스의 리디렉션 URL로 교체해야 합니다.
           render={(renderProps) => (
-            <button onClick={renderProps.onClick} >
-              <img src="src/assets/image/applelogo.png"/>
+            <button onClick={renderProps.onClick}>
             </button>
           )}
         />
+        
       </div>
     </div>
   );
