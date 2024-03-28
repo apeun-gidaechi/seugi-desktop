0c53e887c3f8172c7717b85c929fbf7a5d8d8bd8
jaeuk16 <141097687+jaeuk16@users.noreply.github.com>
2024-03-28 20:07:13 +0900

    ⚡ :: OAuth 회원가입 뷰 구현

--- src/components/router.tsx
+++ src/components/router.tsx
@@ -1,5 +1,7 @@
 import Login from '../pages/Login/login';
 import Selectingjob from '../pages/Signup/Selectjob/selectingjob';
+import Oauthsignup from '../pages/Signup/OAuthsignup/oauthsignup';
+
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const Router = () => {
@@ -7,7 +9,8 @@ const Router = () => {
         <BrowserRouter>
             <Routes>
                 <Route path="/login" element={<Login/>}/>
-                <Route path='selectjob' element={<Selectingjob/>}/>
+                <Route path='/selectjob' element={<Selectingjob/>}/>
+                <Route path='oauthsignup' element={<Oauthsignup/>}/>
             </Routes>
         </BrowserRouter>
     )
