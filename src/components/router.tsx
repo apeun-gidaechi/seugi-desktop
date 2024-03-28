bd0ca5d508d43a48e5e6a7930c431dc74a4caa89
jaeuk16 <141097687+jaeuk16@users.noreply.github.com>
2024-03-28 23:35:59 +0900

    ⚡ :: 이메일 회원가입 뷰 구현

--- src/components/router.tsx
+++ src/components/router.tsx
@@ -1,6 +1,7 @@
 import Login from '../pages/Login/login';
 import Selectingjob from '../pages/Signup/Selectjob/selectingjob';
 import Oauthsignup from '../pages/Signup/OAuthsignup/oauthsignup';
+import Emailsignup from '../pages/Signup/Emailsignup/emailsignup';
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
@@ -10,7 +11,8 @@ const Router = () => {
             <Routes>
                 <Route path="/login" element={<Login/>}/>
                 <Route path='/selectjob' element={<Selectingjob/>}/>
-                <Route path='oauthsignup' element={<Oauthsignup/>}/>
+                <Route path='/oauthsignup' element={<Oauthsignup/>}/>
+                <Route path='/emailsignup' element={<Emailsignup/>}/>
             </Routes>
         </BrowserRouter>
     )
