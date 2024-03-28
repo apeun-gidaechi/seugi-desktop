cece7ac69fe84570c07269e0eac68aaa6cdaf9c6
jaeuk16 <141097687+jaeuk16@users.noreply.github.com>
2024-03-28 20:07:13 +0900

    ⚡ :: OAuth 회원가입 뷰 구현

---
+++ src/pages/Signup/OAuthsignup/oauthsignup.style.ts
@@ -0,0 +1,133 @@
+import styled from 'styled-components';
+
+export const Frame297 = styled.div`
+    display: flex;
+    width: 485px;
+    padding: 36px 32px;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 32px;
+
+    border-radius: 36px;
+    background: #FFF;
+
+    box-shadow: 0px 4px 12px 0px rgba(0, 0, 0, 0.06);
+`;
+
+export const Frame298 = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    align-self: stretch;
+`;
+
+export const Title1 = styled.p`
+color: var(--Black, #000);
+
+font-family: Pretendard;
+font-size: 28px;
+font-style: normal;
+font-weight: 700;
+line-height: 130%;
+`;
+
+export const Frame300 = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+gap: 16px;
+align-self: stretch;
+`;
+
+export const Frame121 = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: flex-start;
+gap: 4px;
+align-self: stretch;
+`;
+
+export const Frame121_1 = styled.div`
+display: flex;
+padding-left: 4px;
+justify-content: center;
+align-items: flex-start;
+gap: 10px;
+`;
+export const Subtitlename = styled.p`
+    color: var(--Black, #000);
+
+    font-family: Pretendard;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 130%; /* 18.2px */
+`;
+
+export const Redstar = styled.a`
+color: var(--Red-Red500, #F90707);
+
+font-family: Pretendard;
+font-size: 14px;
+font-style: normal;
+font-weight: 600;
+line-height: 130%;
+`;
+
+export const Txtfield = styled.input`
+    display: flex;
+    height: 52px;
+    padding: 17px 276px 14px 16px;
+    align-items: center;
+    align-self: stretch;  
+
+    border-radius: var(--Large, 12px);
+    border: 1px solid var(--Gray-Gray300, #E6E6E6);
+    background: var(--Sub-White, #FFF);
+`;
+
+export const Frame299 = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    gap: 8px;
+    align-self: stretch;
+`;
+
+export const Continuebtn = styled.button`
+    display: flex;
+    height: 54px;
+    padding: 12px 125px;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    align-self: stretch;
+
+    border-radius: var(--Large, 12px);
+    background: var(--Primary-Primary500, #1D93F3);
+`;
+
+export const Frame125 = styled.div`
+    display: flex;
+    padding-left: 4px;
+    justify-content: center;
+    align-items: flex-start;
+    gap: 10px;
+`;
+
+export const Haveemail = styled.a`
+    color: var(--Primary-Primary500, #1D93F3);
+
+    /* Body1 */
+    font-family: Pretendard;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 130%; /* 18.2px */ 
+    text-decoration:none;
+`;
