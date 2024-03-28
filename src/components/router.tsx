f6ff9d925408b9c6528304b4d0ecdaf90242428e
je355 <chaeeehui@gmail.com>
2024-03-28 17:44:23 +0900

    ⚡ :: chat router

--- src/components/router.tsx
+++ src/components/router.tsx
@@ -1,5 +1,6 @@
 import Login from '../pages/Login/login';
 import Selectingjob from '../pages/Signup/selectingjob';
+import ChatMain from '../pages/chat/chat'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const Router = () => {
@@ -8,6 +9,7 @@ const Router = () => {
             <Routes>
                 <Route path="/login" element={<Login/>}/>
                 <Route path='selectjob' element={<Selectingjob/>}/>
+                <Route path='/chat' element={<ChatMain/>}/>
             </Routes>
         </BrowserRouter>
     )
