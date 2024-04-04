a2245659fb92af4119c85ed1c69a24aa7ad1fef7
je355 <chaeeehui@gmail.com>
2024-04-04 17:01:09 +0900

    Revert "🐛 :: 충돌"
    This reverts commit 3e66e1bef871b22933fec8af9af0738ed9802962.

---
+++ src/components/router.tsx
@@ -0,0 +1,21 @@
+import Login from './Login/login';
+import Selectingjob from '../pages/Selectjob/index';
+import Oauthsignup from './Signup/OAuthsignup/oauthsignup';
+import Emailsignup from './Signup/Emailsignup/emailsignup';
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<Login/>}/>
+                <Route path='/selectjob' element={<Selectingjob/>}/>
+                <Route path='/oauthsignup' element={<Oauthsignup/>}/>
+                <Route path='/emailsignup' element={<Emailsignup/>}/>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default Router;
