5ed830e0cf0d5ca464d859ced318db00af12d964
jaeuk16 <141097687+jaeuk16@users.noreply.github.com>
2024-04-08 19:13:59 +0900

    ⚡ :: schoolcode view 구현

--- src/components/SchoolCode/SchoolCode.style.ts
+++ src/components/SchoolCode/SchoolCode.style.ts
@@ -59,15 +59,25 @@ export const InputCodeContainer = styled.div`
 
 export const InputCode = styled.input`
     display: flex;
+    width: 52px;
     height: 52px;
     padding: 17px 0px 14px 0px;
     justify-content: center;
     align-items: center;
     flex: 1 0 0;
+    text-align:center;
 
     border-radius: var(--Large, 12px);
     border: 1px solid var(--Gray-Gray300, #E6E6E6);
     background: var(--Sub-White, #FFF);
+
+    color: var(--Sub-Black, #000);
+
+    font-family: 'Pretendard-Regular', sans-serif;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 130%; /* 20.8px */
 `;
 
 export const ButtonContainer = styled.div`
