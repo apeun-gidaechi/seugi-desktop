718adc7d0e4ef5f340f27c5389c386b878f2659c
해준 <127281057+ftery0@users.noreply.github.com>
2024-04-08 20:55:45 +0900

    feat :: 절대경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
-// https://vitejs.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": `${__dirname}/src`,
+    },
+  },
+});
