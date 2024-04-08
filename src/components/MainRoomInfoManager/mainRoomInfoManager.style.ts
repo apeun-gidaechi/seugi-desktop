2b47662285e7f6c3294788d01c1d97438d816be4
je355 <chaeeehui@gmail.com>
2024-04-08 23:09:18 +0900

    ⚡ :: Main Room Info 컴퍼넌트

---
+++ src/components/MainRoomInfoManager/mainRoomInfoManager.style.ts
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+
+export const MainRoomInfoBox = styled.div`
+    width: 272px;
+    height: 152px;
+
+    display: flex;
+    padding: 16px;
+    flex-direction: column;
+    gap: 8px;
+
+    border-radius: 16px;
+    background: #FFF;
+` 
+
+export const NotificationSet = styled.div`
+    color: var(--Sub-Black, #000);
+    /* Subtitle2 */
+    font-family: 'Pretendard-Regular', sans-serif;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 130%; /* 20.8px */
+
+    padding: 8px 0px;
+`
+
+export const OutChatRoom = styled.div`
+    color: var(--Red-Red500, #F90707);
+    /* Subtitle2 */
+    font-family: 'Pretendard-Regular', sans-serif;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 130%; /* 20.8px */
+
+    padding: 8px 0px;
+
+    flex-direction: column;
+`
---
+++ src/components/MainRoomInfoManager/mainRoomInfomanager.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import * as S from "../MainRoomInfoManager/mainRoomInfoManager.style"
+
+const mainRoomInfomanager = () => {
+  return (
+    <>
+        <S.MainRoomInfoBox>
+            <S.NotificationSet>채팅방 설정</S.NotificationSet>
+            <S.NotificationSet>알람 설정</S.NotificationSet>
+            <S.OutChatRoom>채팅방 나가기</S.OutChatRoom>
+        </S.MainRoomInfoBox>
+    </>
+  )
+}
+
+export default mainRoomInfomanager
