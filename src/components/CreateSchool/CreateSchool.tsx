0ae913cb5abb918754c181723076bccda666896d
jaeuk16 <141097687+jaeuk16@users.noreply.github.com>
2024-04-10 16:49:00 +0900

    ⚡ :: CreateSchool view 구현

---
+++ src/components/CreateSchool/CreateSchool.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import * as S from '@/components/CreateSchool/CreateSchool.style';
+import Button from '../button/continuebutton';
+
+const CreateSchool = () => {
+  return (
+    <S.CreateSchoolMain>
+        <S.CreateSchoolContainer>
+            <S.TitleContainer>
+                <S.Title>새 학교 등록</S.Title>
+            </S.TitleContainer>
+            <S.InputImg></S.InputImg>
+            <S.InputContainer>
+                <S.InputBox>
+                    <S.SubtitleContainer>
+                        <S.Subtitle>학교 이름 <S.RedStar>*</S.RedStar></S.Subtitle>
+                    </S.SubtitleContainer>
+                    <S.TextField></S.TextField>
+                </S.InputBox>
+            </S.InputContainer>
+            <S.ButtonContainer>
+                <Button/>
+            </S.ButtonContainer>
+        </S.CreateSchoolContainer>
+    </S.CreateSchoolMain>
+  )
+}
+
+export default CreateSchool;
