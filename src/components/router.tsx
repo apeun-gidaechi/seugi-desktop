157593194122f539fdda14e43b157be828fd9461
jaeuk16 <141097687+jaeuk16@users.noreply.github.com>
2024-04-10 16:49:00 +0900

    ⚡ :: CreateSchool view 구현

--- src/components/router.tsx
+++ src/components/router.tsx
@@ -3,6 +3,11 @@ import Selectingjob from '@/pages/Selectjob/index';
 import ChatMain from '@/pages/chat/chat' 
 import Oauthsignup from '@/components/Signup/OAuthsignup/oauthsignup';
 import Emailsignup from '@/components/Signup/Emailsignup/emailsignup';
+import SelectSchool from '@/components/SelectingSchool/SelectSchool';
+import SchoolCode from '@/components/SchoolCode/SchoolCode';
+import JoinSuccess from '@/components/JoinSuccess/JoinSuccess';
+import CreateSchool from '@/components/CreateSchool/CreateSchool';
+
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const Router = () => {
@@ -18,6 +23,7 @@ const Router = () => {
                 <Route path='/selectschool' element={<SelectSchool/>}/>
                 <Route path='/schoolcode' element={<SchoolCode/>}/>
                 <Route path='/joinsuccess' element={<JoinSuccess/>}/>
+                <Route path='/createschool' element={<CreateSchool/>}/>
             </Routes>
         </BrowserRouter>
     )
