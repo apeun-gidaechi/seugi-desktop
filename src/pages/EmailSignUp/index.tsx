7cf5191d075d3757009f577d9f264999dd9cefb8
jaeuk16 <141097687+jaeuk16@users.noreply.github.com>
2024-04-11 17:17:11 +0900

    page 정리2

--- src/pages/EmailSignUp/index.tsx
+++
@@ -1,8 +0,0 @@
-import  EmailSignup  from "@/components/Signup/Emailsignup/emailsignup";
-
-const SelectPage = ()=>{
-    return(
-        <EmailSignup/>
-    )
-}
-export default SelectPage;
--- src/pages/Selectjob/index.tsx
+++
@@ -1,8 +0,0 @@
-import  SelectingJob  from "@/components/Signup/Selectjob/selectingjob";
-
-const SelectPage = ()=>{
-    return(
-        <SelectingJob/>
-    )
-}
-export default SelectPage;
