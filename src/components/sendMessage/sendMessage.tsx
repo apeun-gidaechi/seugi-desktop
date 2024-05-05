1258b9ff860672542618ba74b9902df5d4165ea2
je355 <chaeeehui@gmail.com>
2024-05-06 00:25:40 +0900

    ⚡ :: 콘솔 업데이트

--- src/components/sendMessage/sendMessage.tsx
+++ src/components/sendMessage/sendMessage.tsx
@@ -1,23 +1,32 @@
-import * as S from "../sendMessage/sendMessage.style"; 
-import React from 'react' 
+import React, { ChangeEvent } from 'react';
+import * as S from '../sendMessage/sendMessage.style';
 
-import PlusMessageFile from "@/assets/image/chat-components/MessageFile.svg"
-import SendArrow from "@/assets/image/chat-components/SendArrow.svg"
+import PlusMessageFile from '@/assets/image/chat-components/MessageFile.svg';
+import SendArrow from '@/assets/image/chat-components/SendArrow.svg';
 
-const sendMessage = () => {
+const SendMessage: React.FC = () => {
+  const handleMessageChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const message = event.target.value;
+    console.log(message);
+  };
+
   return (
     <>
-        <S.SendMessageWrap>
-            <S.PlustFileButton>
-                <S.PlusMessageFile src={PlusMessageFile}/>
-            </S.PlustFileButton>
-            <S.SendMessage type="text" placeholder="메세지 보내기"></S.SendMessage>
-            <S.SendArrowWrap>
-                <S.SendArrow src={SendArrow}/>
-            </S.SendArrowWrap>
-        </S.SendMessageWrap>
+      <S.SendMessageWrap>
+        <S.PlustFileButton>
+          <S.PlusMessageFile src={PlusMessageFile} />
+        </S.PlustFileButton>
+        <S.SendMessage
+          type="text"
+          placeholder="메세지 보내기"
+          onChange={handleMessageChange}
+        />
+        <S.SendArrowWrap>
+          <S.SendArrow src={SendArrow} />
+        </S.SendArrowWrap>
+      </S.SendMessageWrap>
     </>
-  )
-}
+  );
+};
 
-export default sendMessage
+export default SendMessage;
