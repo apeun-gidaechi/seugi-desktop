b62f6d610f9debfd00bff00eadeb3f60ef16abf9
je355 <chaeeehui@gmail.com>
2024-05-08 21:56:47 +0900

    ⚡ :: 콘솔을 찍어봣어요. 히히

--- src/components/sendMessage/sendMessage.tsx
+++ src/components/sendMessage/sendMessage.tsx
@@ -5,11 +5,17 @@ import PlusMessageFile from "@/assets/image/chat-components/MessageFile.svg";
 import SendArrow from "@/assets/image/chat-components/SendArrow.svg";
 import SendArrowBlue from "@/assets/image/chat-components/SendArrowBlue.svg";
 
-const SendMessage = () => {
+const SendMessage: React.FC = () => {
+  const [message, setMessage] = useState("");
   const [isClicked, setIsClicked] = useState(false);
 
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMessage(event.target.value);
+  };
+
   const handleClick = () => {
-    setIsClicked(prevState => !prevState);
+    console.log(message); 
+    setIsClicked(prevState => !prevState); 
   };
 
   return (
@@ -17,16 +23,20 @@ const SendMessage = () => {
         <S.PlustFileButton>
             <S.PlusMessageFile src={PlusMessageFile}/>
         </S.PlustFileButton> 
-        <S.SendMessageInput type="text" placeholder="메세지 보내기"/>
+        <S.SendMessageInput 
+          type="text" 
+          placeholder="메세지 보내기"
+          value={message}
+          onChange={handleChange}
+        />
         <S.SendArrowButton onClick={handleClick}>
           {isClicked ? (
-            <img src={SendArrowBlue} alt="error" /> 
+            <img src={SendArrowBlue} alt="Send Message" /> 
           ) : (
-            <img src={SendArrow} alt="error" /> 
+            <img src={SendArrow} alt="Send Message" /> 
           )}
         </S.SendArrowButton>
     </S.SendMessageWrap>
   );
 }
-export default sendMessage 
-//
+export default SendMessage;
