b005916961da25f1d78ea88ce0eb97e4c3de150e
je355 <chaeeehui@gmail.com>
2024-05-14 20:12:36 +0900

    🐛 :: 소켓 커넥트

--- src/components/sendMessage/socketService.ts
+++ src/components/sendMessage/socketService.ts
@@ -1,29 +1,47 @@
 import Stomp from 'stompjs';
 
-const socket = Stomp.client('wss://hoolc.me/stomp/chat'); 
+let socket: Stomp.Client | undefined;
 
 export const socketService = {
     connect: (token: string) => { 
+        socket = Stomp.client('wss://hoolc.me/stomp/chat'); 
+        
         const headers = {
             'Authorization': `Bearer ${token}`
         };
+        
         socket.connect(headers, function (frame) {
-            socket.subscribe("/exchange/chat.exchange/room.2", function (message) {
+            console.log('소켓 연결됨');
+            socket?.subscribe("/exchange/chat.exchange/room.2", (message: Stomp.Message) => {
                 console.log(message.body);
-            }, headers); 
-        })
+            })
+            // 연결 후에 필요한 작업 수행
+        });
     },
     disconnect: () => {
+        if (!socket) return;
+        
         socket.disconnect(() => {
             console.log('소켓 연결 해제됨');
         });
     },
     sendMessage: (message: string) => {
-        socket.send('/app/chat', {}, message);
+        if (!socket || !socket.connected) {
+            console.error("소켓이 연결되지 않았습니다.");
+            return;
+        }
+
+        socket.send('/pub/chat.message', {}, JSON.stringify({"roomId": 2, "type": "MESSAGE", "message": message, "mention": [], "mentionAll": false}));
     },
-    subscribeToMessages: (callback: (message: string) => void) => {
-        socket.subscribe('/topic/messages', (message: Stomp.Message) => {
+    subscribeToMessages: (topic: string, callback: (message: string) => void) => {
+        if (!socket || !socket.connected) {
+            console.error("소켓이 연결되지 않았습니다.");
+            return;
+        }
+
+        socket.subscribe(topic, (message: Stomp.Message) => {
             callback(message.body);
         });
-    }
+    },
+    
 };
