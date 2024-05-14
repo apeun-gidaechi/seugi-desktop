84f18f2b772928ee653236fb1f33a302f9f51746
je355 <chaeeehui@gmail.com>
2024-05-14 15:10:02 +0900

    🐛 :: 소켓 closed 문제 수정

--- src/components/sendMessage/socketService.ts
+++ src/components/sendMessage/socketService.ts
@@ -1,30 +1,29 @@
 import Stomp from 'stompjs';
 
-const socket = Stomp.client('wss://hoolc.me');
+const socket = Stomp.client('wss://hoolc.me/stomp/chat'); 
 
 export const socketService = {
-  connect: () => {
-    return new Promise<void>((resolve, reject) => {
-      socket.connect({}, () => {
-        console.log('소켓 연결 성공');
-        resolve(); 
-      }, (error) => {
-        console.error('소켓 연결 중 오류 발생:', error);
-        reject(error);
-      });
-    });
-  },
-  disconnect: () => {
-    socket.disconnect(() => {
-      console.log('소켓 연결 해제됨');
-    });
-  },
-  sendMessage: (message: string) => {
-    socket.send('/app/chat', {}, message);
-  },
-  subscribeToMessages: (callback: (message: string) => void) => {
-    socket.subscribe('/topic/messages', (message: Stomp.Message) => {
-      callback(message.body);
-    });
-  }
+    connect: (token: string) => { 
+        const headers = {
+            'Authorization': `Bearer ${token}`
+        };
+        socket.connect(headers, function (frame) {
+            socket.subscribe("/exchange/chat.exchange/room.2", function (message) {
+                console.log(message.body);
+            }, headers); 
+        })
+    },
+    disconnect: () => {
+        socket.disconnect(() => {
+            console.log('소켓 연결 해제됨');
+        });
+    },
+    sendMessage: (message: string) => {
+        socket.send('/app/chat', {}, message);
+    },
+    subscribeToMessages: (callback: (message: string) => void) => {
+        socket.subscribe('/topic/messages', (message: Stomp.Message) => {
+            callback(message.body);
+        });
+    }
 };
