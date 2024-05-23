e3057739299b2edcbcf4f0fe5fa72fdaf4a84b4f
je355 <chaeeehui@gmail.com>
2024-05-23 19:41:34 +0900

    ⚡ :: 채팅방 이름 설정

--- src/components/CreateRoomName/createRoomName.tsx
+++ src/components/CreateRoomName/createRoomName.tsx
@@ -1,25 +1,40 @@
 import * as S from "./createRoomName.style"; 
-import React from 'react'
+import React, { useState } from 'react';
+import CreateRoomCancle from "@/assets/image/chat-components/createroomcancle.svg";
 
-import CreateRoomCancle from "@/assets/image/chat-components/createroomcancle.svg"
+const CreateRoomName: React.FC = () => {
+  const [roomName, setRoomName] = useState("");
 
-const createRoomName = () => {
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setRoomName(e.target.value);
+  };
+
+  const handleCreateButtonClick = () => {
+    console.log(roomName);
+    alert("완료");
+  };
+
   return (
     <div>
-        <S.chatFormWrap>
-            <div style={{display: 'flex', justifyContent: 'space-between'}}> {/* 수정된 div 스타일 */}
-                <S.ChatRoomName>채팅방 이름</S.ChatRoomName>
-                <S.ChatRoomButton>만들기</S.ChatRoomButton>
-            </div>
-            <S.InputRoomNameWrap>
-                <S.InputRoomName>zi존CNs</S.InputRoomName>
-                <S.CreateRoomCancleWrap>
-                    <S.CreateRoomCancle src={CreateRoomCancle}/>
-                </S.CreateRoomCancleWrap>
-            </S.InputRoomNameWrap>
-        </S.chatFormWrap>
+      <S.chatFormWrap>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <S.ChatRoomName>채팅방 이름</S.ChatRoomName>
+          <S.ChatRoomButton onClick={handleCreateButtonClick}>만들기</S.ChatRoomButton>
+        </div>
+        <S.InputRoomNameWrap>
+          <S.InputRoomName
+            type="text"
+            placeholder="채팅방 이름을 설정해주세요"
+            value={roomName}
+            onChange={handleInputChange}
+          />
+          <S.CreateRoomCancleWrap>
+            <S.CreateRoomCancle src={CreateRoomCancle} />
+          </S.CreateRoomCancleWrap>
+        </S.InputRoomNameWrap>
+      </S.chatFormWrap>
     </div>     
-  )
+  );
 }
 
-export default createRoomName
+export default CreateRoomName;
