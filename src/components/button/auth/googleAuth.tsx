8ea5145abbc9c1c61271a377db2e7b6370b08d66
jaeuk16 <141097687+jaeuk16@users.noreply.github.com>
2024-05-23 19:26:45 +0900

    ⚡ :: OAuth

--- src/components/button/auth/googleAuth.tsx
+++ src/components/button/auth/googleAuth.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 
-const googleAuth = () => {
+const GoogleOAuth = () => {
+  const clientId = '1030248081772-slq50ocntpteorj47l6qa90rnh08ukgj.apps.googleusercontent.com';
+
+  const onSuccess = (credentialResponse: any) => {
+    console.log('Login Success:', credentialResponse);
+  };
+
+  const onFailure = () => {
+    console.log('Login Failed');
+  };
+
   return (
-    <div>googleAuth</div>
-  )
-}
+    <GoogleOAuthProvider clientId={clientId}>
+      <div>
+        <GoogleLogin
+          onSuccess={onSuccess}
+          onError={onFailure}
+        />
+      </div>
+    </GoogleOAuthProvider>
+  );
+};
 
-export default googleAuth
+export default GoogleOAuth;
