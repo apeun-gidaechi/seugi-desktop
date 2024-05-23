8749484e351b0b46e27f7d93c124396d40b3a327
je355 <chaeeehui@gmail.com>
2024-05-23 16:58:06 +0900

    design : 안쓰는컴퍼넌트삭제 및 스타일 변경

--- src/components/MainRoomInfoManager/mainRoomInfomanager.tsx
+++ src/components/MainRoomInfoManager/mainRoomInfomanager.tsx
@@ -1,16 +1,44 @@
-import React from 'react'
-import * as S from "@/components/MainRoomInfoManager/mainRoomInfoManager.style"
+import React, { useState } from 'react';
+import * as S from "@/components/MainRoomInfoManager/mainRoomInfoManager.style";
 
 const mainRoomInfomanager = () => {
-  return (
-    <>
-        <S.MainRoomInfoBox>
-            <S.NotificationSet>채팅방 설정</S.NotificationSet>
-            <S.NotificationSet>알람 설정</S.NotificationSet>
-            <S.OutChatRoom>채팅방 나가기</S.OutChatRoom>
-        </S.MainRoomInfoBox>
-    </>
-  )
-}
+    const [isPressed1, setIsPressed1] = useState(false);
+    const [isHovered1, setIsHovered1] = useState(false);
+    const [isPressed2, setIsPressed2] = useState(false);
+    const [isHovered2, setIsHovered2] = useState(false);
+    const [isPressed3, setIsPressed3] = useState(false);
+    const [isHovered3, setIsHovered3] = useState(false);
 
-export default mainRoomInfomanager
+    return (
+        <>
+            <S.MainRoomInfoBox>
+                <S.NotificationSet
+                    onMouseEnter={() => setIsHovered1(true)}
+                    onMouseLeave={() => setIsHovered1(false)}
+                    onClick={() => setIsPressed1(!isPressed1)}
+                    pressed={isPressed1 || isHovered1}
+                >
+                    부관리자 임명
+                </S.NotificationSet>
+                <S.NotificationSet
+                    onMouseEnter={() => setIsHovered2(true)}
+                    onMouseLeave={() => setIsHovered2(false)}
+                    onClick={() => setIsPressed2(!isPressed2)}
+                    pressed={isPressed2 || isHovered2}
+                >
+                    학생 정보 수정
+                </S.NotificationSet>
+                <S.OutChatRoom
+                    onMouseEnter={() => setIsHovered3(true)}
+                    onMouseLeave={() => setIsHovered3(false)}
+                    onClick={() => setIsPressed3(!isPressed3)}
+                    pressed={isPressed3 || isHovered3}
+                >
+                    내보내기
+                </S.OutChatRoom>
+            </S.MainRoomInfoBox>
+        </>
+    );
+};
+
+export default mainRoomInfomanager;
