5fce5be870dc48b676ec9d49efc377320106e9e8
je355 <chaeeehui@gmail.com>
2024-06-10 23:02:41 +0900

    style : 메시지 우측 정렬

--- src/components/MessageBox/messageBox.style.ts
+++ src/components/MessageBox/messageBox.style.ts
@@ -1,12 +1,25 @@
 import styled from "styled-components"; 
 
 
+export const messageContainer = styled.div`
+    display: block;
+    float: right;
+    clear: both; 
+`
+
 export const messageBox = styled.div`
-    background-color: #007BFF; 
     color: white; 
-    padding: 10px; 
     border-radius: 5px; 
     margin: 5px 0; 
    
     width: fit-content; 
+
+    padding: 12px;
+    align-items: flex-start;
+    gap: 10px;
+
+    border-radius: 8px 0px 8px 8px;
+    background: var(--Primary-Primary500, #1D93F3);
+
+    box-shadow: 0px 3px 9px 0px rgba(0, 0, 0, 0.04);
 `
