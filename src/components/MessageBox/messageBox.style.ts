a3ef9cdfaa9bfb20820d7a55a0f7c9d634d3a48f
je355 <chaeeehui@gmail.com>
2024-06-10 21:30:46 +0900

    ⚡ :: 전송 되도록

---
+++ src/components/MessageBox/messageBox.style.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components"; 
+
+
+export const messageBox = styled.div`
+    background-color: #007BFF; 
+    color: white; 
+    padding: 10px; 
+    border-radius: 5px; 
+    margin: 5px 0; 
+   
+    width: fit-content; 
+`
