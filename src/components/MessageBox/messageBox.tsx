503bd1e35a7347b903cb90f020660df7633e8f24
je355 <chaeeehui@gmail.com>
2024-06-12 20:46:55 +0900

    소켓 connect 전송

--- src/components/MessageBox/messageBox.tsx
+++ src/components/MessageBox/messageBox.tsx
@@ -2,19 +2,19 @@ import React from 'react';
 import * as S from "@/components/MessageBox/messageBox.style";
 
 interface MessageBoxProps {
-    message: string;
-    time: string;
+  message: string;
+  time: string;
 }
 
 const MessageBox: React.FC<MessageBoxProps> = ({ message, time }) => {
-    return (
-        <S.messageContainer>
-            <S.messageTime>{time}</S.messageTime>
-            <S.messageBox className="message-box">
-                {message}
-            </S.messageBox>
-        </S.messageContainer>
-    );
+  return (
+    <S.messageContainer>
+      <S.messageTime>{time}</S.messageTime>
+      <S.messageBox className="message-box">
+        {message}
+      </S.messageBox>
+    </S.messageContainer>
+  );
 };
 
 export default MessageBox;
