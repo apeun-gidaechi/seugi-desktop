5841513fff0812ed2eecf139fcb6817c5b05dc56
jaeuk16 <141097687+jaeuk16@users.noreply.github.com>
2024-06-13 19:35:44 +0900

    ⚡ :: 학교 정보 불러오기

--- src/components/JoinSuccess/JoinSuccess.tsx
+++ src/components/JoinSuccess/JoinSuccess.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Button from '@/components/button/Button';
 import * as S from '@/components/JoinSuccess/JoinSuccess.style';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import config from '@/config/config.json';
 
 const JoinSuccess = () => {
@@ -10,7 +10,9 @@ const JoinSuccess = () => {
   const [schoolName, setSchoolName] = useState('');
   const [schoolInfo, setSchoolInfo] = useState('');
   const [schoolImgUrl, setSchoolImgUrl] = useState('');
-
+  const token = window.localStorage.getItem("accessToken");
+  const location = useLocation();
+  const { verificationCode } = location.state || {};
   const handleJoinSuccess = () => {
     navigate('/selectjob');
   };
@@ -18,10 +20,16 @@ const JoinSuccess = () => {
   useEffect(() => {
     const handleSchoolInfo = async () => {
       try {
-        const res = await axios.get(`${config.serverurl}/workspace/`);
-        setSchoolName(res.data.workspaceName);
-        setSchoolInfo(`학생 ${res.data.studentCount}명 선생님 ${res.data.teacherCount}명`);
-        setSchoolImgUrl(res.data.workspaceImageUrl);
+        const res = await axios.get(`${config.serverurl}/workspace/${verificationCode}`, {
+          headers: {
+            Authorization: `${token}`
+          },
+        }
+        );
+        console.log(res.data.data.studentCount);
+        setSchoolName(res.data.data.workspaceName);
+        setSchoolInfo(`학생 ${res.data.data.studentCount}명 선생님 ${res.data.data.teacherCount}명`);
+        setSchoolImgUrl(res.data.data.workspaceImageUrl);
       } catch (error) {
         console.error('Failed to fetch school information:', error);
       }
