e28a2d04202298b0aa6ad6f51c56c8377c04a6b4
jaeuk16 <141097687+jaeuk16@users.noreply.github.com>
2024-06-25 10:07:16 +0900

    ⚡ :: 커스텀axios 적용

--- src/components/JoinSuccess/JoinSuccess.tsx
+++ src/components/JoinSuccess/JoinSuccess.tsx
@@ -1,9 +1,8 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios';
+import SeugiAxios from '@/api/SeugiCutomAxios';
 import Button from '@/components/button/Button';
 import * as S from '@/components/JoinSuccess/JoinSuccess.style';
 import { useNavigate, useLocation } from 'react-router-dom';
-import config from '@/config/config.json';
 
 const JoinSuccess = () => {
   const navigate = useNavigate();
@@ -22,7 +21,7 @@ const JoinSuccess = () => {
   useEffect(() => {
     const handleSchoolInfo = async () => {
       try {
-        const res = await axios.get(`${config.serverurl}/workspace/${verificationCode}`, {
+        const res = await SeugiAxios.get(`/workspace/${verificationCode}`, {
           headers: {
             Authorization: `${token}`
           },
