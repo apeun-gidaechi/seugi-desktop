ca7b0081ba229e18a6db14ad4eb2ff60d7478af8
jaeuk16 <141097687+jaeuk16@users.noreply.github.com>
2024-07-10 09:11:11 +0900

    ⚡ :: 1차

--- src/components/Changeshcool/Changeschool.tsx
+++ src/components/Changeshcool/Changeschool.tsx
@@ -1,14 +1,41 @@
-import React from 'react'
-import * as S from '@/components/Changeshcool/Changeschool.style';
+import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
+
+import axios from 'axios';
+
+import config from '@/constants/config/config.json';
+
+import * as S from '@/components/Changeshcool/Changeschool.style';
+
 import Arrow from '@/assets/image/home/arrow.svg';
 import Setting from '@/assets/image/home/setting_fill.svg';
+
+
 const Changeschool = () => {
+    const [schoolNames, setSchoolNames] = useState<string[]>([]);
     const navigate = useNavigate();
 
     const goCreateSchool = () => {
         navigate('/selectschool')
     }
+
+    useEffect(() => {
+        const fetchSchoolNames = async () => {
+            try {
+                const token = window.localStorage.getItem("accessToken"); 
+                const res = await axios.get(`${config.serverurl}/schools`, {
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    }
+                });
+                setSchoolNames(res.data.schoolNames);
+            } catch (error) {
+                console.error('Error fetching school names:', error);
+            }
+        };
+
+        fetchSchoolNames();
+    }, []);
 
     return (
         <S.CreateSchoolMain>
@@ -26,9 +53,9 @@ const Changeschool = () => {
             <S.Subscribed>
                 <S.SchoolBox>
                     <S.SchoolName>한국디지털어쩌구고등학교</S.SchoolName>
-                    <S.SettingButton>
+                    {/* <S.SettingButton>
                         <S.SettingImg src={Setting} />
-                    </S.SettingButton>
+                    </S.SettingButton> */}
                     <S.ArrowButton >
                         <S.ArrowImg src={Arrow} />
                     </S.ArrowButton>
@@ -38,9 +65,9 @@ const Changeschool = () => {
                 <S.WaitingJoin>가입 대기 증</S.WaitingJoin>
                 <S.SchoolBox>
                     <S.SchoolName>사대부고</S.SchoolName>
-                    <S.SettingButton>
+                    {/* <S.SettingButton>
                         <S.SettingImg src={Setting} />
-                    </S.SettingButton>
+                    </S.SettingButton> */}
                     <S.ArrowButton >
                         <S.ArrowImg src={Arrow} />
                     </S.ArrowButton>
