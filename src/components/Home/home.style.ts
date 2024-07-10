825fdedc97650d631b5dc7fa0055d5f726c4390b
jaeuk16 <141097687+jaeuk16@users.noreply.github.com>
2024-07-10 16:47:53 +0900

    ⚡ :: 전체 점검 수정

--- src/components/Home/home.style.ts
+++ src/components/Home/home.style.ts
@@ -751,3 +751,10 @@ export const ChangeSchool = styled.button`
 
   cursor: pointer;
 `;
+
+export const SearchButton = styled.button`
+  border: none;
+  background: none;
+
+  cursor: pointer;
+`;
