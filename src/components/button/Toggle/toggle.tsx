f5f801532768e9f76c078c975333a8ea5169f4f8
이슬아 <rheeseula@gmail.com>
2024-07-11 21:47:28 +0900

    Temp :: 임시

--- src/components/button/Toggle/toggle.tsx
+++ src/components/button/Toggle/toggle.tsx
@@ -1,23 +1,26 @@
-import React, { useState } from 'react';
-import * as S from "@/components/button/Toggle/toggle.style";
+import React, { useState } from "react";
+import * as S from "@/components/Button/Toggle/toggle.style";
 
 const Toggle = () => {
-    const [latestSort, setLatestSort] = useState(true);
+  const [latestSort, setLatestSort] = useState(true);
 
-    const toggleHandler = () => {
-        setLatestSort((prev) => !prev);
+  const toggleHandler = () => {
+    setLatestSort((prev) => !prev);
+  };
 
-    } 
-
-
-    return (
-        <S.BtnWrapper>
-            <S.CheckBox type='checkbox' id='toggleBtn' onChange={toggleHandler} checked={latestSort} />
-            <S.ButtonLabel htmlFor='toggleBtn' latestSort={latestSort}>
-                <S.ToggleIndicator latestSort={latestSort} />
-            </S.ButtonLabel>
-        </S.BtnWrapper>
-    );
-}
+  return (
+    <S.BtnWrapper>
+      <S.CheckBox
+        type="checkbox"
+        id="toggleBtn"
+        onChange={toggleHandler}
+        checked={latestSort}
+      />
+      <S.ButtonLabel htmlFor="toggleBtn" latestSort={latestSort}>
+        <S.ToggleIndicator latestSort={latestSort} />
+      </S.ButtonLabel>
+    </S.BtnWrapper>
+  );
+};
 
 export default Toggle;
