7287187d32ad6d63ee0205d491755a2febb35321
je355 <chaeeehui@gmail.com>
2024-07-11 23:14:39 +0900

    design : 사이드바, 네브 바 반응형

--- src/components/Navbar/Navbar.style.ts
+++ src/components/Navbar/Navbar.style.ts
@@ -5,7 +5,7 @@ export const SideBarMenu = styled.div`
     position: absolute;
     display: flex;
     width: 80px;
-    height: 1020px;
+    height: 100vh;
     padding: 8px 0px;
     flex-direction: column;
     justify-content: flex-start;
@@ -13,7 +13,17 @@ export const SideBarMenu = styled.div`
     background: #FFF;
     box-shadow: 0px 3px 9px 0px rgba(0, 0, 0, 0.04);
     z-index: 1;
-`
+
+    @media (max-width: 768px) {
+        width: 100%;
+        height: 60px;
+        flex-direction: row;
+        justify-content: space-around;
+        padding: 0 8px;
+        position: fixed;
+        bottom: 0;
+    }
+`;
 
 export const SideBarButton = styled.button<{ $isSelected: boolean }>`
     background: none;
@@ -28,12 +38,15 @@ export const SideBarButton = styled.button<{ $isSelected: boolean }>`
     margin-bottom: 10px;
     position: relative;
     outline: none;
+
     &:focus {
         outline: none;
     }
+
     &:last-child {
         margin-bottom: 0;
     }
+
     &::before {
         content: '';
         position: absolute;
@@ -42,13 +55,30 @@ export const SideBarButton = styled.button<{ $isSelected: boolean }>`
         height: 100%;
         background: ${props => props.$isSelected ? `url(${SelectBar}) no-repeat center` : 'none'};
     }
-`
 
+    @media (max-width: 768px) {
+        width: 60px;
+        height: 100%;
+        margin-bottom: 0;
+
+        &::before {
+            left: auto;
+            top: 0;
+            width: 100%;
+            height: 4px;
+        }
+    }
+`;
 
 export const SideBarImage = styled.img`
     width: 36px;
     height: 36px;
-`
+
+    @media (max-width: 768px) {
+        width: 24px;
+        height: 24px;
+    }
+`;
 
 export const SideAvatarImgWrap = styled.div`
     margin-top: auto;
@@ -56,9 +86,22 @@ export const SideAvatarImgWrap = styled.div`
     justify-content: center;
     align-items: center;
     width: 100%;
-`
+    padding-bottom: 20px;
 
+    @media (max-width: 768px) {
+        margin-top: 0;
+        padding-bottom: 0;
+        position: absolute;
+        bottom: 10px;
+    }
+`;
 
 export const SideAvatarImg = styled.img`
-    margin-bottom: 50px;
-`
+    width: 36px;
+    height: 36px;
+
+    @media (max-width: 768px) {
+        width: 24px;
+        height: 24px;
+    }
+`;
