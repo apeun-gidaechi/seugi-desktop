90f6202c6ae79a09614fcb06d209d70c93b4fdcc
je355 <chaeeehui@gmail.com>
2024-08-12 21:57:22 +0900

    feat :: 경로 별로 타이틀 나오게

--- src/components/common/TitleText/index.style.ts
+++ src/components/common/TitleText/index.style.ts
@@ -1,1 +1,18 @@
 import styled from "styled-components";
+
+export const Title = styled.div`
+    margin-left: 1.5%;
+
+    position: relative;
+
+    width: 100%;
+    height: 100%;
+
+    color: var(--Black, #000);
+
+    font-family: "Pretendard-Regular", sans-serif;
+    font-size: 28px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 130%;
+`
