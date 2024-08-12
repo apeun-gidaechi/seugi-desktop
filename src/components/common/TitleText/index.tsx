a5cfbbf57a6b9ab9cf4be78fc3901995ae11a04d
je355 <chaeeehui@gmail.com>
2024-08-12 21:35:31 +0900

    fix :: 수정 경로 에러 수정

---
+++ src/components/common/TitleText/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import * as S from 'src/components/common/TitleText/index.style'
+
+const index = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default index
