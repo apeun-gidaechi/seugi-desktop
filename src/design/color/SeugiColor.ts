eef76275f05560f6fbd180ff12712e22b0fc3b53
jaeuk16 <141097687+jaeuk16@users.noreply.github.com>
2024-08-12 17:06:05 +0900

    ⚡ :: 디자인시스템(폰트, 컬러)

---
+++ src/design/color/SeugiColor.ts
@@ -0,0 +1,56 @@
+export const SeugiColor = Object.freeze({
+    /*
+    Grayscale
+    */
+    Gray100: '#F4F5F9',
+    Gray200: '#F1F1F1',
+    Gray300: '#E6E6E6',
+    Gray400: '#D1D1D1',
+    Gray500: '#AAAAAA',
+    Gray600: '#787878',
+    Gray700: '#333333',
+    Gray800: '#1D1D1D',
+
+    /*
+    Primary
+     */
+    Primary050: '#F8FCFF',
+    Primary100: '#DCEFFF',
+    Primary200: '#B1DBFD',
+    Primary300: '#7EC4FC',
+    Primary400: '#4BA9F5',
+    Primary500: '#1D93F3',
+    Primary600: '#0481E6',
+    Primary700: '#0A6EC0',
+
+    /*
+    Red
+     */
+    Red100: '#FFF7F7',
+    Red200: '#FFE8E8',
+    Red300: '#FFACAC',
+    Red500: '#F90707',
+
+    /*
+    Orange
+     */
+    Orange100: '#FFF9F7',
+    Orange200: '#FDDDCB',
+    Orange300: '#FFBE99',
+    Orange500: '#F76E33',
+
+    /*
+    Yellow
+     */
+    Yellow100: '#FFFDF7',
+    Yellow200: '#FDF2CB',
+    Yellow300: '#FFE999',
+    Yellow500: '#F7CC33',
+
+    /*
+    Sub
+     */
+    White: '#FFFFFF',
+    Black: '#000000',
+    Transparent: 'rgba(255,255,255,0)'
+});
