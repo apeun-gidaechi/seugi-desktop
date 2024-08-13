1658728de2b333a1f899b82fe23cf8dd006307ef
jaeuk16 <141097687+jaeuk16@users.noreply.github.com>
2024-08-13 21:48:50 +0900

    ⚡ :: navbar height 설정

--- src/components/Navbar/Navbar.style.ts
+++ src/components/Navbar/Navbar.style.ts
@@ -5,7 +5,7 @@ export const SideBarMenu = styled.div`
     position: absolute;
     display: flex;
     width: 80px;
-    height: 115vh;
+    height: 100vh;
     padding: 8px 0px;
     flex-direction: column;
     justify-content: flex-start;
