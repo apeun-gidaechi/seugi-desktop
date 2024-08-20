2a6856067d50a257618007fdfe5e45162c15bdad
jaeuk16 <141097687+jaeuk16@users.noreply.github.com>
2024-08-20 13:55:01 +0900

    ⚡ :: 로그아웃

--- src/components/Profile/SettingProfile/SettingProfile.tsx
+++ src/components/Profile/SettingProfile/SettingProfile.tsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import config from '@/constants/config/config.json';
 import * as S from '@/components/Profile/SettingProfile/SettingProfile.style';
 
 import ProfileImg from '@/assets/image/Profile/settingprofile.svg';
@@ -11,23 +14,54 @@ interface SettingProfileProps {
 }
 
 const SettingProfile: React.FC<SettingProfileProps> = ({ onClose }) => {
+    const workspaceId = window.localStorage.getItem('workspaceId');
+    const token = window.localStorage.getItem('accessToken');
+    const [name, setName] = useState('');
+
+    useEffect(() => {
+        const fetchProfileData = async () => {
+            try {
+                const res = await axios.get(`${config.serverurl}/profile/me?workspaceId=${workspaceId}`, {
+                    headers: {
+                        Authorization: `${token}`,
+                    },
+                });
+                console.log(res.data);
+
+                setName(res.data.data.member.name);
+            } catch (error) {
+                console.error('프로필 데이터를 가져오는데 실패했습니다.', error);
+            }
+        };
+        fetchProfileData();
+    }, [workspaceId, token]);
+
+    const handleLogout = () => {
+        window.localStorage.removeItem('accessToken');
+        window.localStorage.removeItem('workspaceId');
+
+        window.location.href = '/';
+    };
+
     return (
         <S.SettingProfile>
             <S.ProfileContainer>
                 <S.ProfileImgContainer>
-                    <S.ProfileImg src={ProfileImg} /> {/* 수정 */}
+                    <S.ProfileImgButton>
+                        <S.ProfileImg src={ProfileImg} />
+                    </S.ProfileImgButton>
                 </S.ProfileImgContainer>
                 <S.ProfileNameContainer>
                     <S.NameBox>
-                        <S.Name>노영재</S.Name>
+                        <S.Name>{name}</S.Name> 
                         <S.CorrectionButton>
-                            <S.CorrectionButtonImg src={CorrectionImg} />
+                            <S.CorrectionButtonImg src={CorrectionImg} /> 
                         </S.CorrectionButton>
                     </S.NameBox>
                 </S.ProfileNameContainer>
             </S.ProfileContainer>
             <S.TextContainer>
-                <S.ListItem>
+                <S.ListItem onClick={handleLogout}>
                     <S.Text> 로그아웃 </S.Text>
                     <S.ArrowButton>
                         <S.ArrowButtonImg src={Arrow} />
@@ -39,7 +73,7 @@ const SettingProfile: React.FC<SettingProfileProps> = ({ onClose }) => {
                         <S.ArrowButtonImg src={Arrow} />
                     </S.ArrowButton>
                 </S.ListItem>
-                <S.Divider src={Divider} />
+                <S.Divider src={Divider} /> 
                 <S.ListItem>
                     <S.Text> 개인정보 처리 방침 </S.Text>
                     <S.ArrowButton>
@@ -54,7 +88,7 @@ const SettingProfile: React.FC<SettingProfileProps> = ({ onClose }) => {
                 </S.ListItem>
             </S.TextContainer>
         </S.SettingProfile>
-    )
-}
+    );
+};
 
-export default SettingProfile
+export default SettingProfile;
