58a5348cf6008e8845d448a7c715d3f6b02c29c3
jaeuk16 <141097687+jaeuk16@users.noreply.github.com>
2024-08-20 13:30:23 +0900

    ⚡ :: test custom axios

--- src/api/SeugiCutomAxios.tsx
+++ src/api/SeugiCutomAxios.tsx
@@ -1,30 +1,11 @@
-// import axios from 'axios';
-// import config from '@/constants/config/config.json';
+import axios, { AxiosInstance } from 'axios';
+import config from '@/constants/config/config.json';
 
-// const token = window.localStorage.getItem("accessToken");
+const token = window.localStorage.getItem("accessToken");
 
-// const SeugiAxios = axios.create({
-//     baseURL: config.serverurl,
-//     headers: {
-//         'Content-Type': 'application/json',
-//         Authorization: `${token}`
-//     },
-// });
-
-// SeugiAxios.interceptors.request.use((request) => {
-//     if (token) {
-//         request.headers.Authorization = `${token}`;
-//     }
-//     return request;
-// }, (error) => {
-//     return Promise.reject(error);
-// });
-
-// SeugiAxios.interceptors.response.use((res) => {
-//     return res;
-// }, (error) => {
-//     console.error('Error: ', error);
-//     return Promise.reject(error);
-// });
-
-// export default SeugiAxios;
+export const SeugiCustomAxios: AxiosInstance = axios.create({
+    baseURL: `${config.serverurl}`, 
+    headers: {
+        Authorization: `${token}`,
+      },
+});
