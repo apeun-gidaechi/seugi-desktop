dfa450125d973cd9bd53c22e3999389c10895693
jaeuk16 <141097687+jaeuk16@users.noreply.github.com>
2024-08-21 09:44:09 +0900

    ⚡ :: 프로필 이름 변경

--- src/components/Profile/SettingProfile/SettingProfile.tsx
+++ src/components/Profile/SettingProfile/SettingProfile.tsx
@@ -3,11 +3,13 @@ import axios from 'axios';
 
 import config from '@/constants/config/config.json';
 import * as S from '@/components/Profile/SettingProfile/SettingProfile.style';
+import Correction from '@/components/Profile/Correction/Correction';
 
 import ProfileImg from '@/assets/image/Profile/settingprofile.svg';
 import CorrectionImg from '@/assets/image/Profile/CorrectionImg.svg';
 import Arrow from '@/assets/image/Profile/arrow.svg';
 import Divider from '@/assets/image/Profile/ProflieDivider.svg';
+
 
 interface SettingProfileProps {
     onClose: () => void;
@@ -17,6 +19,11 @@ const SettingProfile: React.FC<SettingProfileProps> = ({ onClose }) => {
     const workspaceId = window.localStorage.getItem('workspaceId');
     const token = window.localStorage.getItem('accessToken');
     const [name, setName] = useState('');
+    const [isEditing, setIsEditing] = useState(false);
+
+    const startEditing = (field: any) => {
+        setIsEditing(field);
+    };
 
     useEffect(() => {
         const fetchProfileData = async () => {
@@ -43,50 +50,74 @@ const SettingProfile: React.FC<SettingProfileProps> = ({ onClose }) => {
         window.location.href = '/';
     };
 
+    const handleSave = async (newName: string) => {
+        try {
+            await axios.patch(`${config.serverurl}/profile/${workspaceId}`,
+                { nick: newName },
+                { headers: { Authorization: `${token}` } });
+
+            setName(newName); 
+            setIsEditing(false); 
+        } catch (error) {
+            console.error('이름 저장 실패', error);
+        }
+    };
+
     return (
         <S.SettingProfile>
-            <S.ProfileContainer>
-                <S.ProfileImgContainer>
-                    <S.ProfileImgButton>
-                        <S.ProfileImg src={ProfileImg} />
-                    </S.ProfileImgButton>
-                </S.ProfileImgContainer>
-                <S.ProfileNameContainer>
-                    <S.NameBox>
-                        <S.Name>{name}</S.Name> 
-                        <S.CorrectionButton>
-                            <S.CorrectionButtonImg src={CorrectionImg} /> 
-                        </S.CorrectionButton>
-                    </S.NameBox>
-                </S.ProfileNameContainer>
-            </S.ProfileContainer>
-            <S.TextContainer>
-                <S.ListItem onClick={handleLogout}>
-                    <S.Text> 로그아웃 </S.Text>
-                    <S.ArrowButton>
-                        <S.ArrowButtonImg src={Arrow} />
-                    </S.ArrowButton>
-                </S.ListItem>
-                <S.ListItem>
-                    <S.RText> 회원탈퇴 </S.RText>
-                    <S.ArrowButton>
-                        <S.ArrowButtonImg src={Arrow} />
-                    </S.ArrowButton>
-                </S.ListItem>
-                <S.Divider src={Divider} /> 
-                <S.ListItem>
-                    <S.Text> 개인정보 처리 방침 </S.Text>
-                    <S.ArrowButton>
-                        <S.ArrowButtonImg src={Arrow} />
-                    </S.ArrowButton>
-                </S.ListItem>
-                <S.ListItem>
-                    <S.Text> 서비스 운영 정책 </S.Text>
-                    <S.ArrowButton>
-                        <S.ArrowButtonImg src={Arrow} />
-                    </S.ArrowButton>
-                </S.ListItem>
-            </S.TextContainer>
+            {isEditing ? (
+                <Correction
+                    value="nick" 
+                    content={name}
+                    onSave={handleSave}
+                    onCancel={() => setIsEditing(false)}
+                />
+            ) : (
+                <>
+                    <S.ProfileContainer>
+                        <S.ProfileImgContainer>
+                            <S.ProfileImgButton>
+                                <S.ProfileImg src={ProfileImg} />
+                            </S.ProfileImgButton>
+                        </S.ProfileImgContainer>
+                        <S.ProfileNameContainer>
+                            <S.NameBox>
+                                <S.Name>{name}</S.Name>
+                                    <S.CorrectionButton onClick={() => startEditing('nick')}>
+                                    <S.CorrectionButtonImg src={CorrectionImg} />
+                                </S.CorrectionButton>
+                            </S.NameBox>
+                        </S.ProfileNameContainer>
+                    </S.ProfileContainer>
+                    <S.TextContainer>
+                        <S.ListItem onClick={handleLogout}>
+                            <S.Text> 로그아웃 </S.Text>
+                            <S.ArrowButton>
+                                <S.ArrowButtonImg src={Arrow} />
+                            </S.ArrowButton>
+                        </S.ListItem>
+                        <S.ListItem>
+                            <S.RText> 회원탈퇴 </S.RText>
+                            <S.ArrowButton>
+                                <S.ArrowButtonImg src={Arrow} />
+                            </S.ArrowButton>
+                        </S.ListItem>
+                        <S.Divider src={Divider} />
+                        <S.ListItem>
+                            <S.Text> 개인정보 처리 방침 </S.Text>
+                            <S.ArrowButton>
+                                <S.ArrowButtonImg src={Arrow} />
+                            </S.ArrowButton>
+                        </S.ListItem>
+                        <S.ListItem>
+                            <S.Text> 서비스 운영 정책 </S.Text>
+                            <S.ArrowButton>
+                                <S.ArrowButtonImg src={Arrow} />
+                            </S.ArrowButton>
+                        </S.ListItem>
+                    </S.TextContainer>
+                </>
+            )}
         </S.SettingProfile>
     );
 };
