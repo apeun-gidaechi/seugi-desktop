36f58bdd6b8e0d46bcf9c079b7d7cb19989872da
jaeuk16 <141097687+jaeuk16@users.noreply.github.com>
2024-08-26 13:20:22 +0900

    Notification component 분리

--- src/components/Home/Subscribed/Home.style.ts
+++ src/components/Home/Subscribed/Home.style.ts
@@ -97,137 +97,6 @@ export const HomeWrapper1DownContainer = styled.div`
   align-self: stretch;
 `;
 
-export const LeftContainer = styled.div`
-  display: flex;
-  padding: 12px 12px 16px 12px;
-  flex-direction: column;
-  align-items: flex-start;
-  gap: var(--Corner-Small, 10px);
-  flex: 1 0 0;
-
-  border-radius: 12px;
-  background: ${SeugiColor.White};
-
-  box-shadow: 0px 3px 9px 0px rgba(0, 0, 0, 0.04);
-`;
-
-export const NotificationContainer = styled.div`
-  display: flex;
-  padding: 4px;
-  justify-content: space-between;
-  align-items: center;
-  align-self: stretch;
-`;
-
-export const NotificationTitleContainer = styled.div`
-  display: flex;
-  align-items: center;
-`
-
-export const NotificationBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 8px;
-  align-self: stretch;
-`;
-
-export const NotificationWrapper = styled.div`
-  display: flex;
-  padding: 12px;
-  flex-direction: column;
-  align-items: flex-start;
-  gap: 8px;
-  align-self: stretch;
-
-  border-radius: 8px;
-  background: ${SeugiColor.White};
-
-  box-shadow: 0px 3px 9px 0px rgba(0, 0, 0, 0.04);
-`;
-
-export const NotificationContentAuthor = styled.span`
-  color:${SeugiColor.Gray600};
-
-  ${SeugiFont.body.body2};
-`;
-
-export const NotificationContentTitle = styled.span`
-  position: relative;
-
-  color: ${SeugiColor.Black};
-
-  ${SeugiFont.subtitle.subtitle2};
-`;
-
-export const NotificationContentDescription = styled.span`
-  position: relative;
-
-  color: ${SeugiColor.Black};
-
-  ${SeugiFont.body.body2};
-`;
-
-export const NotificationAddEmoji = styled.img`
-  position: relative;
-
-  width: 28px;
-  height: 28px;
-
-  padding: 4px;
-`;
-
-export const NotificationEmojiBox = styled.div`
-  display: flex;
-
-  flex-direction: row;
-`;
-
-export const NotificationEmojiWrapper = styled.div`
-  position: relative;
-
-  display: flex;
-
-  justify-content: center;
-  align-items: center;
-
-  margin-right: 4px;
-
-  cursor: pointer;
-
-  &.Clicked {
-    border-radius: 8px;
-    border: 1px solid ${SeugiColor.Primary300};
-    background: ${SeugiColor.Primary100};
-  }
-
-  transition: background 0.25s;
-
-  border-radius: 8px;
-  border: 1px solid ${SeugiColor.Gray200};
-  background: ${SeugiColor.Gray100};
-
-  width: 51px;
-  height: 29px;
-
-  position: relative;
-`;
-
-export const NotificationEmoji = styled.img`
-  position: relative;
-
-  width: 15px;
-  height: 16px;
-`;
-
-export const NotificationEmojiCount = styled.span`
-  margin: 3px;
-
-  color: ${SeugiColor.Gray600};
-  ${SeugiFont.body.body1};
-`;
-
 export const TitleContainer = styled.div`
   display: flex;
   padding: 4px;
@@ -349,25 +218,6 @@ export const Item = styled.div`
   }
 `;
 
-export const NotificationLogo = styled.img`
-  width: 32px;
-  height: 32px;
-`;
-
-export const NotificationTitle = styled.h2`
-  position: relative;
-
-  left: 8px;
-
-  color: ${SeugiColor.Black};
-
-  ${SeugiFont.subtitle.subtitle2};
-`;
-
-export const NArrowLogo = styled.img`
-  position: relative;
-`;
-
 export const ArrowLogo = styled.img`
   position: relative;
 `;
@@ -397,7 +247,6 @@ export const ScheduleDivBox = styled.div`
   border-radius: 12px;
   flex-direction: column;
 `
-
 export const ArrowLButton = styled.button`
   background: none;
   border: none;
