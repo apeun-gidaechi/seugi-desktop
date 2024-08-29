cb42a7016e3892f9963dfb7b993ef30bd238b20d
je355 <chaeeehui@gmail.com>
2024-08-29 19:23:54 +0900

    fix :: 사이드바가왜두개

--- src/pages/chat/chat.tsx
+++ src/pages/chat/chat.tsx
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "@/pages/chat/chat.style";
-import SideBar from "@/components/common/ChatSidebar/Chat/index";
-import TopButton from '@/components/button/chatButton/index';
-import SendMessage from "@/components/common/sendMessage/sendMessage";
-import ChatRoom from '@/components/common/chatRoom/unSelect/index';
-
+import SideBar from "@/components/common/sidebar/sidebar";
+import UnChatRoom from '@/components/common/chatRoom/unSelect/index';
+import Selected from '@/components/common/chatRoom/Selected/index';
 import Layer from '@/components/layer/chat/index';
 
-const Chat = () => {
+const Chat: React.FC = () => {
+  const [selectedChatRoom, setSelectedChatRoom] = useState<string | null>(null);
+
+  const handleSelectChatRoom = (room: string) => {
+    setSelectedChatRoom(room);
+    console.log("Selected chat room:", room)
+  };
+
   return (
     <S.ChatContainer>
-      <Layer/>
-      <ChatRoom/>
+      <Layer />
+      <SideBar onSelectChatRoom={handleSelectChatRoom} /> 
+      {selectedChatRoom ? (
+        <Selected roomName={selectedChatRoom} />
+      ) : (
+        <UnChatRoom />
+      )}
     </S.ChatContainer>
   );
 };
