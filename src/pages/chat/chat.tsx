843568f9a819f657edf83fb3508b7bc982684250
je355 <chaeeehui@gmail.com>
2024-09-02 23:29:09 +0900

    comment :: 주석 변경

--- src/pages/chat/chat.tsx
+++ src/pages/chat/chat.tsx
@@ -3,7 +3,7 @@ import * as S from "@/pages/chat/chat.style";
 import UnChatRoom from '@/components/common/chatRoom/unSelect/index';
 import SelectedChatRoom from '@/components/common/chatRoom/Selecte/index';
 import Layer from '@/components/layer/chat/index';
-import Sidebar from '@/components/common/sidebar/sidebar'; // Import Sidebar component
+import Sidebar from '@/components/common/sidebar/sidebar'; 
 
 const Chat = () => {
   const [selectedChatRoom, setSelectedChatRoom] = useState<string | null>(null);
@@ -15,7 +15,7 @@ const Chat = () => {
   return (
     <S.ChatContainer>
       <Layer />
-      <Sidebar onSelectChatRoom={handleSelectChatRoom} /> {/* Pass handleSelectChatRoom as prop */}
+      <Sidebar onSelectChatRoom={handleSelectChatRoom} /> 
       {selectedChatRoom ? (
         <SelectedChatRoom room={selectedChatRoom} />
       ) : (
