71593ac611f5fdc9e538a835ee86ac0074815876
je355 <chaeeehui@gmail.com>
2024-09-03 17:39:33 +0900

    design :: fix 배열

--- src/pages/chat/chat.style.ts
+++ src/pages/chat/chat.style.ts
@@ -1,10 +1,17 @@
 import styled from "styled-components";
 
 export const ChatContainer = styled.div`
+background: var(--Primary-Primary050, #F8FCFF);
     display: flex;
     flex-direction: column;
     height: 100vh;
     justify-content: space-between;
     position: relative;
     width: 100vw;
+`;
+
+export const ButtonWrapper = styled.div`
+    position: absolute;
+    top: 0;
+    right: 0;
 `;
