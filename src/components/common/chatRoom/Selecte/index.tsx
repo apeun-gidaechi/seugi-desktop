99364d5c9763035895dc7344197cf79fada8ea6e
je355 <chaeeehui@gmail.com>
2024-09-03 20:52:06 +0900

    fix :: 사이드바 사라짐 issue 해결

--- src/components/common/chatRoom/Selecte/index.tsx
+++ src/components/common/chatRoom/Selecte/index.tsx
@@ -3,16 +3,19 @@ import React from 'react'
 import * as S from '@/components/common/chatRoom/unSelect/index.style'
 
 import SelectChatRoom from '@/assets/image/chat/sadErrorImg.svg';
-
+import SendMessage from '../../sendMessage/sendMessage';
+import Sidebar from '../../sidebar/sidebar';
 
 interface SelectedChatRoomProps {
   room: string;
+  currentUser: string;
 }
 
-const SelectedChatRoom: React.FC<SelectedChatRoomProps> = ({ room }) => {
+const SelectedChatRoom: React.FC<SelectedChatRoomProps> = ({ room, currentUser }) => {
   return (
     <div>
       <h1>Chat Room: {room}</h1>
+      <SendMessage chatRoom={room} currentUser={currentUser} />
     </div>
   );
 };
