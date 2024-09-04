892a5d2926493717daa9be2fe649ad68132b86d5
jaeuk16 <141097687+jaeuk16@users.noreply.github.com>
2024-09-04 11:21:55 +0900

    ⚡ :: 공지 생성하기 컴퍼넌트

---
+++ src/components/Home/Notification/CreateNotice/CreateNotice.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import * as S from '@/components/Home/Notification/CreateNotice/CreateNotice.style';
+
+const CreateNotice = () => {
+    return (
+        <S.CreateNoticeContainer>
+            <S.TitleContainer>
+                <S.Title>새 알림 작성</S.Title>
+                <S.TitleButton>업로드</S.TitleButton>
+            </S.TitleContainer>
+            <S.InputContainer>
+                <S.TitleInputField
+                    placeholder='제목을 입력해 주세요'
+                />
+                <S.ContentInputField 
+                    placeholder='내용을 입력해 주세요'
+                />
+            </S.InputContainer>
+        </S.CreateNoticeContainer>
+    )
+}
+
+export default CreateNotice
