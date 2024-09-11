67f9b0bb4d9e4fe5d505c46018e11f12974d01e0
je355 <chaeeehui@gmail.com>
2024-09-12 01:11:48 +0900

    file :: hook 분리

--- src/components/common/sendMessage/sendMessage.tsx
+++ src/components/common/sendMessage/sendMessage.tsx
@@ -1,9 +1,8 @@
-import React, { useState, useEffect, useRef } from "react";
-import { socketService } from './socketService'; 
-import axios from "axios";
+import React, { useState, useRef } from "react";
+import useChatMessages from '@/hooks/SendMessage/useChatMessages';
+import useFileUpload from '@/hooks/SendMessage/useFileUpload';
 import * as S from "@/components/common/sendMessage/sendMessage.style";
 import MessageBox from "@/components/MessageBox/messageBox";
-
 import PlusMessageFile from "@/assets/image/chat-components/MessageFile.svg";
 import SendArrow from "@/assets/image/chat-components/SendArrow.svg";
 import SendArrowBlue from "@/assets/image/chat-components/sendBlueArrow.svg";
@@ -23,13 +22,13 @@ interface SendMessageProps {
 const SendMessage: React.FC<SendMessageProps> = ({ chatRoom, currentUser }) => {
   const [message, setMessage] = useState("");
   const [hasText, setHasText] = useState(false);
-  const [receivedMessages, setReceivedMessages] = useState<
-    { message: string; time: string; sender: string; type: FileType }[]
-  >([]);
   const [showDropdown, setShowDropdown] = useState(false);
-
+  
   const fileInputRef = useRef<HTMLInputElement | null>(null);
   const imageInputRef = useRef<HTMLInputElement | null>(null);
+
+  const { receivedMessages, sendMessage } = useChatMessages(chatRoom, currentUser);
+  const { uploadFile } = useFileUpload(chatRoom, currentUser, sendMessage);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const text = event.target.value;
@@ -37,107 +36,35 @@ const SendMessage: React.FC<SendMessageProps> = ({ chatRoom, currentUser }) => {
     setHasText(!!text);
   };
 
-  const sendMessage = (message: string, type: FileType = FileType.FILE) => {
-    const time = new Date().toISOString();
-    const newMessage = {
-      roomId: chatRoom,
-      type: type,
-      message: message,
-      mention: [],
-      mentionAll: false,
-      emoticon: "",
-      time: time,
-      sender: currentUser,
-    };
-    socketService.sendMessage(JSON.stringify(newMessage), chatRoom); // Pass roomId here
-    setReceivedMessages((prevMessages) => [
-      ...prevMessages,
-      { message: message, time: time, sender: currentUser, type: type },
-    ]);
-    setMessage("");
-    setHasText(false);
-  };
-
   const handleClick = () => {
     if (message.trim() !== "") {
       sendMessage(message, FileType.FILE);
+      setMessage("");
+      setHasText(false);
     }
   };
 
   const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === "Enter" && message.trim() !== "") {
-      sendMessage(message, FileType.FILE); 
-    }
-  };
-
-  const uploadFile = async (file: File, type: FileType) => {
-    const formData = new FormData();
-    formData.append("file", file);
-  
-    try {
-      const response = await axios.post(`/upload/${type}`, formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      });
-  
-      const fileUrl = response.data;
-      const time = new Date().toISOString();
-  
-      const newMessageForServer = {
-        roomId: chatRoom,
-        type: type,
-        message: fileUrl,
-        mention: [],
-        mentionAll: false,
-        emoticon: "",
-      };
-  
-      const newMessageForState = {
-        message: fileUrl,
-        time: time,
-        sender: currentUser,
-        type: type,
-      };
-  
-      socketService.sendMessage(JSON.stringify(newMessageForServer), chatRoom); // Pass roomId here
-  
-      setReceivedMessages((prevMessages) => [...prevMessages, newMessageForState]);
-    } catch (error) {
-      console.error("File upload failed:", error);
+      sendMessage(message, FileType.FILE);
+      setMessage("");
+      setHasText(false);
     }
   };
 
   const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
     if (file) {
-      const fileType = FileType.FILE;
-      uploadFile(file, fileType);
+      uploadFile(file, FileType.FILE);
     }
   };
 
   const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     const image = event.target.files?.[0];
     if (image) {
-      const fileType = FileType.IMG;
-      uploadFile(image, fileType);
+      uploadFile(image, FileType.IMG);
     }
   };
-
-  useEffect(() => {
-    socketService.connect();
-
-    // Correct subscription topic format
-    const topic = `/exchange/chat.exchange/room.${chatRoom}`;
-    socketService.subscribeToMessages(topic, (message) => {
-      const newMessage = JSON.parse(message);
-      setReceivedMessages((prevMessages) => [...prevMessages, newMessage]);
-    });
-
-    return () => {
-      socketService.unsubscribeFromMessages(topic); // Ensure to unsubscribe on cleanup
-    };
-  }, [chatRoom]);
 
   return (
     <div>
--- src/components/common/sidebar/sidebar.tsx
+++ src/components/common/sidebar/sidebar.tsx
@@ -1,91 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
+import { useLocation } from "react-router-dom";
 import * as S from "@/components/common/ChatSidebar/Chat/index.style";
 import SearchIcon from "@/assets/image/chat-components/Search.svg";
 import AvatarProfile from "@/assets/image/chat-components/Avatar.svg";
 import Navbar from "@/components/common/Navbar/Navbar";
 import TitleText from "@/components/common/TitleText/index";
-import config from "@/constants/ChatMember/config.json";
+import CreateRoomBtn from "@/assets/image/sidebar/add_fill.svg";
+import useChatSidebar from "@/hooks/Sidebar/useChatSidebar";
 
 interface SidebarProps {
   onSelectChatRoom: (room: string) => void;
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ onSelectChatRoom }) => {
-  const [searchText, setSearchText] = useState("");
-  const [chatRooms, setChatRooms] = useState<string[]>([]);
-
-  const handleSearch = async () => {
-    console.log("Search triggered with:", searchText);
-    if (searchText.trim() !== "") {
-      const roomFound = chatRooms.includes(searchText) || config.name.includes(searchText);
-
-      if (roomFound) {
-        console.log("found room");
-        
-        handleChatRoomClick(searchText);
-      } else {
-        await createRoom(searchText);
-      }
-
-      setSearchText("");
-    }
-  };
-
-  const createRoom = async (roomName: string) => {
-    console.log("Creating room:", roomName);
-    try {
-      const requestData = {
-        workspaceId: "669e339593e10f4f59f8c583",
-        roomName: roomName, 
-        joinUsers: [10], 
-        chatRoomImg: "" 
-      };
-  
-      const response = await fetch('https://api.seugi.com/chat/personal/create', { 
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwiZW1haWwiOiJhZG1pbkBhZG1pbi5jb20iLCJyb2xlIjoiUk9MRV9BRE1JTiIsImlhdCI6MTcyNjA2NDc0NywiZXhwIjoxNzI2MDcwNzQ3fQ.UHfrc2EM-3C5efG-aQy_ROE4KwpxflrqH2nIv7qr6i8"
-        },
-        body: JSON.stringify(requestData),
-      });
-  
-      const result = await response.text(); 
-  
-      console.log("API response:", result);
-  
-      if (response.ok) {
-        console.log("Room created successfully:", roomName);
-        addChatRoom(roomName);
-        handleChatRoomClick(roomName);
-      } else {
-        console.error(`Error creating room: ${result}`);
-      }
-    } catch (error) {
-      if (error instanceof Error) {
-        console.error(`An error occurred: ${error.message}`);
-      } else {
-        console.error('An unknown error occurred');
-      }
-    }
-  };
-
-  const addChatRoom = (roomName: string) => {
-    console.log("Adding room to state:", roomName);
-    setChatRooms((prevRooms) => {
-      console.log("Previous rooms:", prevRooms); 
-      if (!prevRooms.includes(roomName)) {
-        const updatedRooms = [...prevRooms, roomName];
-        console.log("Updated rooms:", updatedRooms); 
-        return updatedRooms;
-      }
-      return prevRooms;
-    });
-  };
-
-  const handleChatRoomClick = (room: string) => {
-    onSelectChatRoom(room);
-  };
+  const location = useLocation();
+  const { searchText, setSearchText, chatRooms, handleSearch, handleChatRoomClick } = useChatSidebar(onSelectChatRoom);
 
   return (
     <>
@@ -107,7 +36,12 @@ const Sidebar: React.FC<SidebarProps> = ({ onSelectChatRoom }) => {
                 }
               }}
             />
-            <S.SearchIcon src={SearchIcon} onClick={handleSearch} />
+            <S.IconWrapper>
+              <S.SearchIcon src={SearchIcon} onClick={handleSearch} />
+              {location.pathname === '/groupchat' && (
+                <S.PlusButtonImg src={CreateRoomBtn} alt="Create Room" onClick={handleSearch} />
+              )}
+            </S.IconWrapper>
           </S.SideFinder>
           <S.ChatRoomsWrap>
             <S.ChatRoomList>
