36c66d4b80b7ec137db37c2677935ceafddd1be5
je355 <chaeeehui@gmail.com>
2024-09-12 17:18:24 +0900

    fix :: 소켓이러 수정

--- src/components/common/chatRoom/Select/index.tsx
+++ src/components/common/chatRoom/Select/index.tsx
@@ -5,24 +5,38 @@ import * as S from '@/components/common/chatRoom/Select/index.style'
 import SelectChatRoom from '@/assets/image/chat/sadErrorImg.svg';
 import SendMessage from '../../sendMessage/sendMessage';
 import Sidebar from '../../sidebar/sidebar';
+import MessageBox from "@/components/MessageBox/messageBox";
+import useChatMessages from '@/hooks/SendMessage/useChatMessages';
 
 interface SelectedChatRoomProps {
   room: string;
   currentUser: string;
 }
 
+interface SendMessageProps {
+  chatRoom: string;
+  currentUser: string;
+}
+
 const SelectedChatRoom: React.FC<SelectedChatRoomProps> = ({ room, currentUser }) => {
+  const { receivedMessages } = useChatMessages(room, currentUser); // Move receivedMessages here
+  
   return (
     <S.AllWrapContainer>
-    <S.ContainerWrapper>
+      <S.ContainerWrapper>
         <S.Container>
+          <div>
+            {receivedMessages.map((msg, index) => (
+              <MessageBox key={index} message={msg.message} time={msg.time} />
+            ))}
+          </div>
         </S.Container>
-    </S.ContainerWrapper>
-    <div>
-      <SendMessage chatRoom={room} currentUser={currentUser} />
-    </div>
-  </S.AllWrapContainer>
+      </S.ContainerWrapper>
+      <div>
+        <SendMessage chatRoom={room} currentUser={currentUser} />
+      </div>
+    </S.AllWrapContainer>
   );
 };
 
-export default SelectedChatRoom;
+export default SelectedChatRoom
