9be334a4ab44f585cd5b378873a8ad3e006e48f8
jaeuk16 <141097687+jaeuk16@users.noreply.github.com>
2024-09-12 22:43:42 +0900

    학교 코드 훅 분리

--- src/components/SchoolCode/SchoolCode.tsx
+++ src/components/SchoolCode/SchoolCode.tsx
@@ -1,51 +1,23 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import * as S from "@/components/SchoolCode/SchoolCode.style";
 import Button from "@/components/Button/Button";
 import CodeTextField from "@/components/CodeTextField/CodeTextFeild";
-import { useNavigate } from "react-router-dom";
+
 import Backimg from "@/assets/image/Backimg.svg";
 
-import { SeugiCustomAxios } from "@/api/SeugiCutomAxios";
 import Session from "@/util/TokenExpired/TokenExpired";
 import { clearAccessToken } from "@/api/SeugiCutomAxios";
 
+import useSchoolCode from '@/hooks/Schoolcode/index';
+
 const SchoolCode = () => {
-  const navigate = useNavigate();
-  const [code, setCode] = useState<string[]>(Array(6).fill(""));
-  const token = window.localStorage.getItem("accessToken");
-
-  useEffect(() => {
-    document.body.style.overflow = "hidden";
-    return () => {
-      document.body.style.overflow = "auto";
-    };
-  }, []);
-
-  const handleContinue = async () => {
-    const verificationCode = code.join("");
-
-    try {
-      const res = await SeugiCustomAxios.get(`/workspace?code=${verificationCode}`);
-      console.log("Code sent successfully:", res.data);
-      navigate("/joinsuccess", { state: { verificationCode } });
-    } catch (error) {
-      console.error("Error sending code:", error);
-    }
-  };
-
-  const handleCodeChange = (updatedCode: string[]) => {
-    setCode(updatedCode);
-  };
-
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter") {
-      handleContinue();
-    }
-  };
-
-  const Backclick = () => {
-    navigate("/selectschool");
-  };
+  const {
+    token,
+    Backclick,
+    handleKeyDown,
+    handleCodeChange,
+    handleContinue
+  } = useSchoolCode();
 
   return (
     <S.SchoolCodeMain>
