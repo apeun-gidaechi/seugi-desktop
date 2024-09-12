7f3a7c0580c96f978160783827963c1a80094c71
jaeuk16 <141097687+jaeuk16@users.noreply.github.com>
2024-09-12 23:28:07 +0900

    경로 수정

--- src/Components/router.tsx
+++ src/Components/router.tsx
@@ -4,14 +4,13 @@ import SchoolCode from '@/Pages/SchoolcodePage/SchoolCodePage';
 import JoinSuccess from '@/Pages/JoinSuccessPage/JoinSuccessPage';
 import Login from '@/Pages/Login/LoginPage';
 import Selectingjob from '@/Pages/Selectjob/SelectJobPage';
-import ChatMain from '@/Pages/Chat/chat'
-import Oauthsignup from '@/pages/AuthSignup/AuthSignupPage';
+import ChatMain from '@/Pages/chat/chat'
 import Emailsignup from '@/Pages/EmailSignUp/EmailSignUpPage';
 import CreateSchool from '@/Pages/CreateSchoolPage/CreateSchoolPage';
 import Authentication from '@/Pages/EmailAuthentication/EmailAuthenticationPage';
 import WaitingJoin from '@/Pages/WaitingJoin/WaitingJoinPage';
 import Home from '@/Pages/Home/home'
-import Chat from '@/Pages/Chat/chat'
+import Chat from '@/Pages/chat/chat'
 import GroupChat from '@/Pages/GroupChat/index'
 import UnHome from '@/Components/Home/NotSubscribed/UnHome';
 import React from 'react';
