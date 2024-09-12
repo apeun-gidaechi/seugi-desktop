595bb0e65baa07f58ab2db46715448e5788fab62
이슬아 <rheeseula@gmail.com>
2024-09-12 14:02:42 +0900

    feat :: LoginPage GoogleOAuthProvider 추가

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,8 +1,12 @@
-import  Login  from "@/components/Login/login";
+import Login from "@/components/Login/login";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import config from "@/constants/config/config.json";
 
-const LoginPage = ()=>{
-    return(
-        <Login/>
-    )
-}
-export default LoginPage; 
+const LoginPage = () => {
+  return (
+    <GoogleOAuthProvider clientId={config["client-id"]}>
+      <Login />
+    </GoogleOAuthProvider>
+  );
+};
+export default LoginPage;
