2d321a0403e9daaa760bd5a7c2984fb85dfd93d4
jaeuk16 <141097687+jaeuk16@users.noreply.github.com>
2024-09-13 13:54:53 +0900

    ♻ :: onBoarding 훅 분리

--- src/Components/OnBording/Signup/EmailAuthentication/EmailAuthentication.tsx
+++ src/Components/OnBording/Signup/EmailAuthentication/EmailAuthentication.tsx
@@ -6,24 +6,13 @@ import Cloud1 from '@/assets/image/onbording/EmailAuthentication/cloud1.svg';
 import Cloud2 from '@/assets/image/onbording/EmailAuthentication/cloud2.svg';
 import Cloud3 from '@/assets/image/onbording/EmailAuthentication/cloud3.svg';
 import Backimg from '@/assets/image/Backimg.svg'
-import CodeTextField from '@/Components/OnBording/CodeTextField/CodeTextFeild';
+import CodeTextField from '@/Components/OnBording/CodeTextField/CodeTextField';
 import CustomAlert from '@/Components/Alert/Alert';
 
 import useAuthentication from '@/Hooks/EmailAuthenticationHook/index';
 
 const EmailAuthentication = () => {
-    const {
-        timer,
-        showAlert,
-        isCodeSent,
-        handleCloseAlert,
-        handleSendCode,
-        handleCodeChange,
-        formatTime,
-        sendCode,
-        handleKeyDown,
-        Backclick
-    } = useAuthentication();
+    const { ...Authentication } = useAuthentication();
 
     return (
         <S.AuthenticationMain>
@@ -33,7 +22,7 @@ const EmailAuthentication = () => {
             <S.Sun src={Sun} />
             <S.AuthenticationContainer>
                 <S.TitleContainer>
-                    <S.BackButton onClick={Backclick}>
+                    <S.BackButton onClick={Authentication.Backclick}>
                         <S.BackImg src={Backimg} />
                     </S.BackButton>
                     <S.Title>이메일 인증</S.Title>
@@ -46,28 +35,28 @@ const EmailAuthentication = () => {
                     </S.CodeInputContainer>
                     <S.InputBox>
                         <CodeTextField
-                            onChange={handleCodeChange}
-                            onKeyDown={handleKeyDown}
+                            onChange={Authentication.handleCodeChange}
+                            onKeyDown={Authentication.handleKeyDown}
                         />
                     </S.InputBox>
-                    {timer > 0 ? (
-                        <S.TimerSpan>{formatTime(timer)} 남음</S.TimerSpan>
+                    {Authentication.timer > 0 ? (
+                        <S.TimerSpan>{Authentication.formatTime(Authentication.timer)} 남음</S.TimerSpan>
                     ) : (
-                        <S.CodeSpan onClick={handleSendCode}>
-                            {isCodeSent ? '인증 코드 재전송' : '인증 코드 전송'}
+                        <S.CodeSpan onClick={Authentication.handleSendCode}>
+                            {Authentication.isCodeSent ? '인증 코드 재전송' : '인증 코드 전송'}
                         </S.CodeSpan>
                     )}
-                    {showAlert &&
+                    {Authentication.showAlert &&
                         <CustomAlert
                             position='top-right'
                             titletext="인증코드를 전송했어요"
                             subtext="이메일 함을 확인해 보세요"
-                            onClose={handleCloseAlert}
+                            onClose={Authentication.handleCloseAlert}
                         />
                     }
                 </S.CodeContainer>
                 <S.ContinueContainer>
-                    <Button text='계속하기' onClick={sendCode} />
+                    <Button text='계속하기' onClick={Authentication.sendCode} />
                 </S.ContinueContainer>
             </S.AuthenticationContainer>
         </S.AuthenticationMain>
