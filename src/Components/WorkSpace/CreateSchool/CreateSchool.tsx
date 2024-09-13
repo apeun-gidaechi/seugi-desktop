818b65d09607ff06802a4986e4cdfc4af53875c2
jaeuk16 <141097687+jaeuk16@users.noreply.github.com>
2024-09-13 13:40:41 +0900

    ♻ :: 워크스페이스 훅 분리

--- src/Components/WorkSpace/CreateSchool/CreateSchool.tsx
+++ src/Components/WorkSpace/CreateSchool/CreateSchool.tsx
@@ -1,116 +1,33 @@
-import React, { useState, ChangeEvent, useEffect } from 'react';
+import React from 'react';
 import * as S from './CreateSchool.style';
 import Button from '@/Components/Button/Button';
 import TextField from '@/Components/common/TextField/TextField';
-import createSchoolImg from '@/assets/image/join-school/createshoolimg.svg';
 import Backimg from '@/assets/image/Backimg.svg';
 import PlusButtonimg from '@/assets/image/join-school/plus.svg';
-import { useNavigate } from 'react-router-dom';
-// import config from '@/constants/config/config.json';
-
 import Session from '@/Util/TokenExpired/TokenExpired';
-import { clearAccessToken, SeugiCustomAxios } from '@/Api/SeugiCutomAxios';
+import { clearAccessToken } from '@/Api/SeugiCutomAxios';
+
+import useCreateSchool from '@/Hooks/CreateSchool/index';
 
 const CreateSchool = () => {
-    const navigate = useNavigate();
-    const token = window.localStorage.getItem("accessToken");
-    const [workspaceName, setWorkspaceName] = useState<string>('');
-    const [workspaceImageUrl, setWorkspaceImageUrl] = useState<string | null>(null);
-
-    const isworkspaceImg = workspaceImageUrl ? workspaceImageUrl : createSchoolImg;
-
-    useEffect(() => {
-        document.body.style.overflow = 'hidden';
-        return () => {
-            document.body.style.overflow = 'auto';
-        };
-    }, []);
-
-    const handleCreateSchool = async () => {
-        if (!workspaceName.trim()) {
-            alert('학교 이름을 입력해주세요.');
-            return;
-        }
-
-        try {
-            const res = await SeugiCustomAxios.get(`/workspace/`);
-            console.log(res);
-
-            if (res.data.data.length > 0) {
-                navigate('/home');
-            } else {
-                navigate('/unhome');
-            }
-        } catch (error) {
-            if (isAxiosError(error)) {
-                if (error.response && error.response.status === 401) {
-                    clearAccessToken();
-                    navigate('/');
-                } else {
-                    console.error('Error sending code:', error.response?.data);
-                }
-            } else {
-                console.error('Error:', error);
-            }
-        }
-    };
-
-    const handleChangeImage = async (e: ChangeEvent<HTMLInputElement>) => {
-        if (!e.target.files || e.target.files.length === 0) {
-            console.warn('No files selected');
-            return;
-        }
-
-        const formData = new FormData();
-        formData.append('type', 'IMG');
-        formData.append('file', e.target.files[0]);
-
-        try {
-            const res = await SeugiCustomAxios.post(`/file/upload/IMG`, formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                },
-            });
-            console.log('Image uploaded', res.data);
-            setWorkspaceImageUrl(res.data.data);
-        } catch (error) {
-            if (isAxiosError(error)) {
-                if (error.response && error.response.status === 401) {
-                    clearAccessToken();
-                    navigate('/');
-                } else {
-                    console.error('Error uploading image:', error.response?.data);
-                }
-            } else {
-                console.error('Error:', error);
-            }
-        }
-    };
-
-    const isAxiosError = (error: unknown): error is { response: { status: number, data: any } } => {
-        return (error as any).isAxiosError === true;
-    };
-
-    const Backclick = () => {
-        navigate('/selectschool');
-    };
+    const { ...CreateSchool } = useCreateSchool();
 
     return (
         <S.CreateSchoolMain>
-            <Session token={token} clearAccessToken={clearAccessToken} />
+            <Session token={CreateSchool.token} clearAccessToken={clearAccessToken} />
             <S.CreateSchoolContainer>
                 <S.TitleContainer>
-                    <S.BackButton onClick={Backclick}>
+                    <S.BackButton onClick={CreateSchool.Backclick}>
                         <S.BackImg src={Backimg} />
                     </S.BackButton>
                     <S.Title>새 학교 등록</S.Title>
                 </S.TitleContainer>
                 <S.ButtonContainer>
                     <S.UpLoadButtonLabel htmlFor="upload-button">
-                        <S.ButtonImg src={isworkspaceImg} style={{ zIndex: workspaceImageUrl ? 2 : 1 }} $isImageUploaded={!!workspaceImageUrl} />
+                        <S.ButtonImg src={CreateSchool.isworkspaceImg} style={{ zIndex: CreateSchool.workspaceImageUrl ? 2 : 1 }} $isImageUploaded={!!CreateSchool.workspaceImageUrl} />
                         <S.PlusButton src={PlusButtonimg} />
                     </S.UpLoadButtonLabel>
-                    <S.Input id="upload-button" type="file" onChange={handleChangeImage} accept='image/png, image/jpeg, image/jpg' />
+                    <S.Input id="upload-button" type="file" onChange={CreateSchool.handleChangeImage} accept='image/png, image/jpeg, image/jpg' />
                 </S.ButtonContainer>
                 <S.InputContainer>
                     <S.InputBox>
@@ -120,13 +37,13 @@ const CreateSchool = () => {
                         <TextField
                             style={{ border: "1px solid var(--Gray-Gray300, #E6E6E6)" }}
                             placeholder='학교 이름을 입력해주세요'
-                            value={workspaceName}
-                            onChange={(e) => setWorkspaceName(e.target.value)}
+                            value={CreateSchool.workspaceName}
+                            onChange={(e) => CreateSchool.setWorkspaceName(e.target.value)}
                         />
                     </S.InputBox>
                 </S.InputContainer>
                 <S.ButtonContainer>
-                    <Button onClick={handleCreateSchool} text='계속하기' />
+                    <Button onClick={CreateSchool.handleCreateSchool} text='계속하기' />
                 </S.ButtonContainer>
             </S.CreateSchoolContainer>
         </S.CreateSchoolMain>
--- src/Components/WorkSpace/WaitingJoin/WaitingJoin.tsx
+++ src/Components/WorkSpace/WaitingJoin/WaitingJoin.tsx
@@ -9,19 +9,16 @@ import Session from '@/Util/TokenExpired/TokenExpired';
 import useWaitingJoin from '@/Hooks/WaitingJoin/index';
 
 const WaitingJoin = () => {
-    const {
-        token,
-        handleWaitingJoin
-    } = useWaitingJoin();
+    const { ...WaitingJoin } = useWaitingJoin();
 
     return (
         <S.WaitingAcceptanceFrame>
-            <Session token={token} clearAccessToken={clearAccessToken} />
+            <Session token={WaitingJoin.token} clearAccessToken={clearAccessToken} />
             <S.WaitingAcceptanceContainer>
                 <S.SchoolInfoContainer>
                     <S.SchoolImg src={schoolimg} />
                     <S.SchoolName>학교 가입 신청 완료</S.SchoolName>
-                    <Button onClick={handleWaitingJoin} text="완료" />
+                    <Button onClick={WaitingJoin.handleWaitingJoin} text="완료" />
                 </S.SchoolInfoContainer>
                 <S.MentContainer>
                     <S.MentImg src={ment} />
