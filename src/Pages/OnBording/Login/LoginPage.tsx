4f6c2342dafc1ab98b639b6d881ea737b184943a
jaeuk16 <141097687+jaeuk16@users.noreply.github.com>
2024-09-19 11:30:57 +0900

    페이지 정리

--- src/Pages/OnBording/Login/LoginPage.tsx
+++ src/Pages/OnBording/Login/LoginPage.tsx
@@ -1,6 +1,7 @@
-import Login from "@/Components/OnBording/SignIn/login";
+import Login from "@/Components/Onboarding/SignIn/login";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import config from "@/constants/config/config.json";
+import React from 'react'
 
 // config에 타입 선언
 const typedConfig = config as { "client-id": string };
