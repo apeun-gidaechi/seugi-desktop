4c56f4bb2258a806bd1a2002b29796271435e7d8
jaeuk16 <141097687+jaeuk16@users.noreply.github.com>
2024-09-23 12:02:59 +0900

    unhome 과제뷰

--- src/Components/Home/NotSubscribed/UnHome.style.ts
+++ src/Components/Home/NotSubscribed/UnHome.style.ts
@@ -434,3 +434,73 @@ export const SchoolDetail = styled.span`
 
   margin-left:9vw;
 `
+
+export const AssignmentMain = styled.div`
+    display: flex;
+    padding: 12px 12px 16px 12px;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 12px;
+    align-self: stretch;
+
+    border-radius: 12px;
+    background: ${SeugiColor.White};
+
+    box-shadow: 0px 3px 9px 0px rgba(0, 0, 0, 0.04);
+`
+
+export const AssignmentTitleBox = styled.div`
+  display: flex;
+  padding: 4px;
+  align-items: center;
+  gap: 8px;
+  align-self: stretch;
+`
+
+export const AssignmentImg = styled.img`
+  width: 28px;
+  height: 28px;
+`
+
+export const AssignmentTitleText = styled.span`
+  color: ${SeugiColor.Black};
+
+  ${SeugiFont.subtitle.subtitle2};
+`
+
+export const AssignmentBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  align-self: stretch;
+`
+
+export const PostAssignment = styled.span`
+  color: ${SeugiColor.Black};
+
+  ${SeugiFont.body.body1};
+
+  margin-right:1rem;
+`
+
+export const AssignmentButton = styled.a`
+    display: flex;
+    padding: 12px 12px 16px 12px;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 12px;
+    align-self: stretch;
+
+    border:none;
+    background:none;
+
+    cursor: pointer;
+
+    text-decoration:none;
+`
+
+export const AssignmentButtonText = styled.span`
+    color: ${SeugiColor.Black};
+
+  ${SeugiFont.subtitle.subtitle2};
+`
