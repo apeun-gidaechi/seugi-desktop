5d414642463220727be7cd1cedf2fc9fcf29b512
je355 <chaeeehui@gmail.com>
2024-10-17 23:41:59 +0900

    Revert "token '' 상태"
    This reverts commit 8559dd1bcca22789bdbdc264c1144c1ac5cb6dce.

--- src/Hooks/OnBording/LoginHook/index.ts
+++ src/Hooks/OnBording/LoginHook/index.ts
@@ -196,7 +196,7 @@ const index = () => {
             if (authorization && authorization.code) {
                 const appleAuthResponse = await axios.post(`${SERVER_URL}/oauth/apple/authenticate`, {
                     code: authorization.code,
-                    token: '',
+                    token: fcmToken,
                     platform: "WEB",
                     name: response.user?.name
                 });
