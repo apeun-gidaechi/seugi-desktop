28fd1df5468b3810973ed37811762971db30df87
je355 <chaeeehui@gmail.com>
2024-10-18 21:18:48 +0900

    Revert "!HOTFIX :: 맘지막이되길바라며"
    This reverts commit 7d011c0369bd17a5a810a7395a5a9a47fdaff9c5.

--- vite.config.ts
+++ vite.config.ts
@@ -12,10 +12,10 @@ export default defineConfig(({ mode }) => {
     server: {
       host: '0.0.0.0',
       // 개발 환경에서만 https 설정 적용
-      // https: mode === 'development' ? {
-      //   key: fs.readFileSync(`${__dirname}/certificates/localhost-key.pem`),  // 로컬에서 HTTPS 키 파일 경로
-      //   cert: fs.readFileSync(`${__dirname}/certificates/localhost.pem`),      // 로컬에서 HTTPS 인증서 파일 경로
-      // } : false,  // 배포 환경에서는 HTTPS 설정 없음
+      https: mode === 'development' ? {
+        key: fs.readFileSync(`${__dirname}/certificates/localhost-key.pem`),  // 로컬에서 HTTPS 키 파일 경로
+        cert: fs.readFileSync(`${__dirname}/certificates/localhost.pem`),      // 로컬에서 HTTPS 인증서 파일 경로
+      } : false,  // 배포 환경에서는 HTTPS 설정 없음
     },
     plugins: [react()],
     resolve: {
