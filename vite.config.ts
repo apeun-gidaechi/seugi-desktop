f0c1fc659d6268265f6dddc8a9d3546d7545ad6e
je355 <chaeeehui@gmail.com>
2024-10-18 18:15:53 +0900

    !HOTFIX :: ssl

--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,26 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
-import fs from 'fs'; // 파일 시스템 모듈을 사용하여 인증서 파일을 읽기 위함
+import fs from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-export default defineConfig({
-  server: {
-    host: '0.0.0.0',
-    https: {
-      key: fs.readFileSync(`${__dirname}/certificates/localhost-key.pem`),  // 로컬에서 HTTPS 키 파일 경로
-      cert: fs.readFileSync(`${__dirname}/certificates/localhost.pem`),      // 로컬에서 HTTPS 인증서 파일 경로
-    }
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": `${__dirname}/src`,
+export default defineConfig(({ mode }) => {
+  return {
+    server: {
+      host: '0.0.0.0',
+      // 개발 환경에서만 https 설정 적용
+      https: mode === 'development' ? {
+        key: fs.readFileSync(`${__dirname}/certificates/localhost-key.pem`),  // 로컬에서 HTTPS 키 파일 경로
+        cert: fs.readFileSync(`${__dirname}/certificates/localhost.pem`),      // 로컬에서 HTTPS 인증서 파일 경로
+      } : false,  // 배포 환경에서는 HTTPS 설정 없음
     },
-  },
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": `${__dirname}/src`,
+      },
+    },
+  };
 });
