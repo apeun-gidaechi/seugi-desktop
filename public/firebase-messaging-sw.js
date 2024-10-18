ce3fcc6b6aca0e9aa6ceb20310654cc4540b68f6
je355 <chaeeehui@gmail.com>
2024-10-18 19:57:58 +0900

    !HOTFIX :: env error

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,17 +2,24 @@
 importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
 
-const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID,
-    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-};
+const params = new URL(location).searchParams;
+const apiKey = params.get("apiKey");
+const authDomain = params.get("authDomain");
+const projectId = params.get("projectId");
+const storageBucket = params.get("storageBucket");
+const messagingSenderId = params.get("messagingSenderId");
+const appId = params.get("appId");
+const measurementId = params.get("measurementId");
 
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp({
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
+  measurementId: measurementId,
+});
 
 const messaging = firebase.messaging();
 
