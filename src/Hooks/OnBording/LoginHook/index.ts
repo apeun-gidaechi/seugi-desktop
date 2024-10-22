43541924f0a4db58d43f7b8c6b415ae5efd90afb
jaeuk16 <141097687+jaeuk16@users.noreply.github.com>
2024-10-22 23:55:57 +0900

    scope 2개

--- src/Hooks/OnBording/LoginHook/index.ts
+++ src/Hooks/OnBording/LoginHook/index.ts
@@ -117,14 +117,6 @@ const index = () => {
         "https://www.googleapis.com/auth/classroom.coursework.students.readonly",
     ];
 
-    const scopes = [
-        "email",
-        "profile",
-        "https://www.googleapis.com/auth/classroom.courses.readonly",
-        "https://www.googleapis.com/auth/classroom.coursework.me.readonly",
-        "https://www.googleapis.com/auth/classroom.coursework.students.readonly",
-    ];
-
     const handleGoogleLogin = useGoogleLogin({
         flow: "auth-code",
         scope: scopes.join(" "),
