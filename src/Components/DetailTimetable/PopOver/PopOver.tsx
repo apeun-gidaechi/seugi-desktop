32f650e65a98a752036e73d766457a85c743a881
jaeuk16 <141097687+jaeuk16@users.noreply.github.com>
2024-10-22 19:01:02 +0900

    popover 창

--- src/Components/DetailTimetable/PopOver/PopOver.tsx
+++ src/Components/DetailTimetable/PopOver/PopOver.tsx
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react';
 import * as S from './PopOver.style';
+import ModifyTimetable from '@/Components/DetailTimetable/ModifyTimetable/ModifyTimetable';
+import DeleteTimetable from '@/components/DetailTimetable/DeleteTimetable/DeleteTimetable';
 
 const PopOver = () => {
+    const [activeComponent, setActiveComponent] = useState<string | null>(null);
+
+    const handleModifyClick = () => {
+        setActiveComponent('modify');
+    };
+
+    const handleDeleteClick = () => {
+        setActiveComponent('delete');
+    };
+
     return (
         <S.PopUpDiv>
-            <S.Button>
+            <S.Button onClick={handleModifyClick}>
                 <S.ModitySpan>시간표 수정</S.ModitySpan>
             </S.Button>
-            <S.Button>
+            <S.Button onClick={handleDeleteClick}>
                 <S.DeleteSpan>시간표 삭제</S.DeleteSpan>
             </S.Button>
+
+            {activeComponent === 'modify' && <ModifyTimetable />}
+            {activeComponent === 'delete' && <DeleteTimetable />}
         </S.PopUpDiv>
-    )
+    );
 }
 
-export default PopOver
+export default PopOver;
