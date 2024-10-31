5277845840181b3d73a0b78b2f8ac18402b85c45
jaeuk16 <141097687+jaeuk16@users.noreply.github.com>
2024-10-31 16:38:54 +0900

    Update index.ts

--- src/Hooks/OnBording/LoginHook/index.ts
+++ src/Hooks/OnBording/LoginHook/index.ts
@@ -159,12 +159,11 @@ const index = () => {
             },
         });
     };
+
     useEffect(() => {
         const handleSuccess = async (response: any) => {
-            console.log("1");
             const code = response.detail.authorization.code;
             const name = response.user?.name;
-            console.log(response);
             try {
                 const token = await axios.post(`${SERVER_URL}/oauth/apple/authenticate`, {
                     code,
